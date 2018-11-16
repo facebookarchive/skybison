456b9f822a85e9e10761b9f90af931451fa079c0
Eddie Elizondo <eelizondo@fb.com>
2018-11-15 18:52:00 -0800

    [Pyro][noop] Extensions: Add placeholders for PyExceptions objects and functions
    Summary: These placeholders are required in order to link in some functions required for PyType_Ready. There is still some missing framework in the runtime before these functions can be fully implemented.
    
    Based on Facebook D8126702

---
+++ ext/Objects/exceptions.cpp
@@ -0,0 +1,7 @@
+// exceptions.c implementation
+
+#include "Python.h"
+
+PyObject* PyExc_OverflowError;
+PyObject* PyExc_TypeError;
+PyObject* PyExc_SystemError;
---
+++ ext/Python/errors.cpp
@@ -0,0 +1,25 @@
+// errors.c implementation
+
+#include "runtime/utils.h"
+
+#include "Python.h"
+
+void PyErr_SetString(PyObject*, const char*) {
+  UNIMPLEMENTED("PyErr_SetString");
+}
+
+PyObject* PyErr_Occurred(void) {
+  UNIMPLEMENTED("PyErr_Ocurred");
+}
+
+PyObject* PyErr_Format(PyObject*, const char*, ...) {
+  UNIMPLEMENTED("PyErr_Format");
+}
+
+void PyErr_Clear(void) {
+  UNIMPLEMENTED("PyErr_Clear");
+}
+
+int PyErr_BadArgument(void) {
+  UNIMPLEMENTED("PyErr_BadArgument");
+}
