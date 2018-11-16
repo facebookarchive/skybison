8496e2004852aa318c56115d0d7d5630b39d7460
Eddie Elizondo <eelizondo@fb.com>
2018-11-15 18:52:00 -0800

    [Pyro] Utils: Patch CPython sources accessing slots
    Summary: Update the CPython source patcher to move slots accessors to using the api -> PyType_GetSlot
    
    Based on Facebook D9354371

--- util/patch_cpython_sources.py
+++ util/patch_cpython_sources.py
@@ -49,13 +49,181 @@ object_to_type_map = {
 
 def object_to_type(lines, cpython_file):
     for sr in object_to_type_map.values():
-        if (sr.file_filter or "") in cpython_file:
+        if not sr.file_filter or sr.file_filter in cpython_file:
+            lines = re.sub(sr.regex, sr.repl, lines)
+    return lines
+
+
+slot_to_func_type_map = {
+    "tp_dealloc": "destructor",
+    "tp_print": "printfunc",
+    "tp_getattr": "getattrfunc",
+    "tp_setattr": "setattrfunc",
+    "tp_repr": "reprfunc",
+    "tp_hash": "hashfunc",
+    "tp_call": "ternaryfunc",
+    "tp_str": "reprfunc",
+    "tp_getattro": "getattrofunc",
+    "tp_setattro": "setattrofunc",
+    "tp_traverse": "traverseproc",
+    "tp_clear": "inquiry",
+    "tp_richcompare": "richcmpfunc",
+    "tp_iter": "getiterfunc",
+    "tp_iternext": "iternextfunc",
+    "tp_descr_set": "descrgetfunc",
+    "tp_descr_get": "descrsetfunc",
+    "tp_alloc": "allocfunc",
+    "tp_init": "initproc",
+    "tp_new": "newfunc",
+    "tp_free": "freefunc",
+    "tp_is_gc": "inquiry",
+    "tp_del": "destructor",
+    "tp_finalize": "destructor",
+    "nb_add": "binaryfunc",
+    "nb_subtract": "binaryfunc",
+    "nb_multiply": "binaryfunc",
+    "nb_remainder": "binaryfunc",
+    "nb_divmod": "binaryfunc",
+    "nb_power": "ternaryfunc",
+    "nb_negative": "unaryfunc",
+    "nb_positive": "unaryfunc",
+    "nb_absolute": "unaryfunc",
+    "nb_bool": "inquiry",
+    "nb_invert": "unaryfunc",
+    "nb_lshift": "binaryfunc",
+    "nb_rshift": "binaryfunc",
+    "nb_and": "binaryfunc",
+    "nb_xor": "binaryfunc",
+    "nb_or": "binaryfunc",
+    "nb_int": "unaryfunc",
+    "nb_float": "unaryfunc",
+    "nb_inplace_add": "binaryfunc",
+    "nb_inplace_subtract": "binaryfunc",
+    "nb_inplace_multiply": "binaryfunc",
+    "nb_inplace_remainder": "binaryfunc",
+    "nb_inplace_power": "ternaryfunc",
+    "nb_inplace_lshift": "binaryfunc",
+    "nb_inplace_rshift": "binaryfunc",
+    "nb_inplace_and": "binaryfunc",
+    "nb_inplace_xor": "binaryfunc",
+    "nb_inplace_or": "binaryfunc",
+    "nb_floor_divide": "binaryfunc",
+    "nb_true_divide": "binaryfunc",
+    "nb_inplace_floor_divide": "binaryfunc",
+    "nb_inplace_true_divide": "binaryfunc",
+    "nb_index": "unaryfunc",
+    "nb_matrix_multiply": "binaryfunc",
+    "nb_inplace_matrix_multiply": "binaryfunc",
+    "sq_length": "lenfunc",
+    "sq_concat": "binaryfunc",
+    "sq_repeat": "ssizeargfunc",
+    "sq_item": "ssizeargfunc",
+    "sq_ass_item": "ssizeobjargproc",
+    "sq_contains": "objobjproc",
+    "sq_inplace_concat": "binaryfunc",
+    "sq_inplace_repeat": "ssizeargfunc",
+    "mp_length": "lenfunc",
+    "mp_subscript": "binaryfunc",
+    "mp_ass_subscript": "objobjargproc",
+    "am_await": "unaryfunc",
+    "am_aiter": "unaryfunc",
+    "am_anext": "unaryfunc",
+    "bf_getbuffer": "getbufferproc",
+    "bf_releasebuffer": "releasebufferproc",
+}
+
+
+def replace_type_to_slot_match(match):
+    # Check if a the accessor is via '->' or '.'
+    ref = "&" if match.group("ref") == "." else ""
+    return (
+        f"PyType_GetSlot({ref}{match.group('type')}, Py_{match.group('slot')})"
+    )
+
+
+def replace_slot_to_callable_match(match):
+    # Find the correct casting type for the slot
+    func_type = slot_to_func_type_map[match.group("func_type")]
+    return f"(({func_type}){match.group('void_ptr')}){match.group('extra_paren')}{match.group('args')}"
+
+
+def replace_type_to_member_match(match):
+    slot = match.group("slot")
+    # Check if a the accessor is via '->' or '.'
+    ref = "&" if match.group("ref") == "." else ""
+    # Special handle tp_name and tp_dict to access through GetAttr
+    attr_accessors = ["tp_name", "tp_dict", "tp_base", "tp_doct"]
+    if slot in attr_accessors:
+        if "tp_name" in slot:
+            return f"((const char*)PyObject_GetAttrString({ref}{match.group('type')}, \"__name__\"))"
+        return f"PyObject_GetAttrString({ref}{match.group('type')}, \"__{slot[3:]}__\")"
+    return replace_type_to_slot_match(match)
+
+
+slot_to_macro_map = {
+    # Match variables that are accessing a slot
+    # i.e: type->slot->nb_add => PyType_GetSlot(type, Py_nb_add)
+    "type_to_slot_regex": SourceRegex(
+        regex=re.compile(
+            r"(?P<type>[a-zA-Z0-9_]+)(?P<ref>->|\.)(?P<slot_type>[a-zA-Z0-9_]+)->(?P<slot>(nb|sq|mp|am)_[a-z_]+)",
+            re.MULTILINE,
+        ),
+        repl=replace_type_to_slot_match,
+        file_filter=None,
+    ),
+    # Match variables that are accessing a slot through Py_TYPE
+    # i.e: Py_TYPE(o)->slot->nb_add => PyType_GetSlot(Py_TYPE(o), Py_nb_add)
+    "type_macro_to_slot_regex": SourceRegex(
+        regex=re.compile(
+            r"(?P<type>Py_TYPE\([a-zA-Z0-9_]+\))(?P<ref>->|\.)(?P<slot_type>[a-zA-Z0-9_]+)->(?P<slot>(nb|sq|mp|am)_[a-z_]+)",
+            re.MULTILINE,
+        ),
+        repl=replace_type_to_slot_match,
+        file_filter=None,
+    ),
+    # Match variables that are accessing a type slot
+    # i.e: type->tp_doc => PyType_GetSlot(type, Py_tp_doc)
+    "type_to_member_regex": SourceRegex(
+        regex=re.compile(
+            r"(?P<type>[a-zA-Z0-9_]+)(?P<ref>->|\.)(?P<slot>tp_[a-z_]+)",
+            re.MULTILINE,
+        ),
+        repl=replace_type_to_member_match,
+        file_filter=None,
+    ),
+    # Match variables that are accessing a type slot through Py_TYPE
+    # i.e: Py_TYPE(o)->tp_doc => PyType_GetSlot(Py_TYPE(o), Py_tp_doc)
+    "type_macro_to_member_regex": SourceRegex(
+        regex=re.compile(
+            r"(?P<type>Py_TYPE\([a-zA-Z0-9_]+\))(?P<ref>->|\.)(?P<slot>tp_[a-z_]+)",
+            re.MULTILINE,
+        ),
+        repl=replace_type_to_member_match,
+        file_filter=None,
+    ),
+    # Cast the slots that are doing a function call to the function type
+    # i.e: PyType_GetSlot(type, Py_tp_alloc)(v, w) -> ((allocfunc)PyType_GetSlot(type, Py_tp_alloc))(v, w)
+    "slot_to_callable_regex": SourceRegex(
+        regex=re.compile(
+            r"(?P<deref>\*)?(?P<void_ptr>PyType_GetSlot\(.*?, Py_(?P<func_type>[a-z_]*)\))(?P<extra_paren>.*)(?P<args>\(.*\))",
+            re.MULTILINE,
+        ),
+        repl=replace_slot_to_callable_match,
+        file_filter=None,
+    ),
+}
+
+
+def slot_to_macro(lines, cpython_file):
+    for sr in slot_to_macro_map.values():
+        if not sr.file_filter or sr.file_filter in cpython_file:
             lines = re.sub(sr.regex, sr.repl, lines)
     return lines
 
 
 def modify_cpython_source(source_dir):
     filter_source_files = lambda p: p.endswith(".c") or p.endswith(".h")
+    file_transforms = [object_to_type, slot_to_macro]
     for subdir, _dirs, files in os.walk(source_dir):
         list_of_files = [os.path.join(subdir, x) for x in files]
 
@@ -65,7 +233,9 @@ def modify_cpython_source(source_dir):
                 original_lines = f.read()
 
             # Run Transformations
-            lines = object_to_type(original_lines, cpython_file)
+            lines = original_lines
+            for t in file_transforms:
+                lines = t(lines, cpython_file)
 
             # Save File
             if lines != original_lines:
--- util/patch_cpython_sources_test.py
+++ util/patch_cpython_sources_test.py
@@ -75,5 +75,154 @@ if (Py_TYPE(self) == &type) {
         self.assertEqual(res, expected_lines)
 
 
+class TestSlotAccess(unittest.TestCase):
+    def test_two_chain_slot_to_macro(self):
+        lines = """
+foo->slot->nb_int
+foo_bar->slot->sq_length(o)
+foobaz->slot->mp_subscript
+baz->slot->am_await(self)
+"""
+        expected_lines = """
+PyType_GetSlot(foo, Py_nb_int)
+PyType_GetSlot(foo_bar, Py_sq_length)(o)
+PyType_GetSlot(foobaz, Py_mp_subscript)
+PyType_GetSlot(baz, Py_am_await)(self)
+"""
+        res = pcs.slot_to_macro(lines, "")
+        self.assertEqual(res, expected_lines)
+
+    def test_two_chain_slot_to_ref_type(self):
+        lines = """
+PyFloat_Type.tp_as_number->nb_power(v, w, x);
+"""
+        expected_lines = """
+((ternaryfunc)PyType_GetSlot(&PyFloat_Type, Py_nb_power))(v, w, x);
+"""
+        res = pcs.slot_to_macro(lines, "")
+        self.assertEqual(res, expected_lines)
+
+    def test_two_chain_slot_to_macro(self):
+        lines = """
+Py_TYPE(foo)->slot->nb_int
+Py_TYPE(bar)->slot->sq_length(o)
+Py_TYPE(baz)->slot->mp_subscript
+Py_TYPE(foobar)->slot->am_await(self)
+"""
+        expected_lines = """
+PyType_GetSlot(Py_TYPE(foo), Py_nb_int)
+((lenfunc)PyType_GetSlot(Py_TYPE(bar), Py_sq_length))(o)
+PyType_GetSlot(Py_TYPE(baz), Py_mp_subscript)
+((unaryfunc)PyType_GetSlot(Py_TYPE(foobar), Py_am_await))(self)
+"""
+        res = pcs.slot_to_macro(lines, "")
+        self.assertEqual(res, expected_lines)
+
+    def test_slot_to_callable(self):
+        lines = """
+return PyType_GetSlot(&foo, Py_nb_invert)(v);
+return PyType_GetSlot(&foo, Py_nb_lshift)(v, w);
+return PyType_GetSlot(&foo, Py_nb_power)(v, w, z);
+"""
+        expected_lines = """
+return ((unaryfunc)PyType_GetSlot(&foo, Py_nb_invert))(v);
+return ((binaryfunc)PyType_GetSlot(&foo, Py_nb_lshift))(v, w);
+return ((ternaryfunc)PyType_GetSlot(&foo, Py_nb_power))(v, w, z);
+"""
+        res = pcs.slot_to_macro(lines, "")
+        self.assertEqual(res, expected_lines)
+
+    def test_slot_to_deref_callable(self):
+        lines = """
+return *PyType_GetSlot(&foo, Py_nb_invert)(v);
+return *PyType_GetSlot(&foo, Py_nb_lshift)(v, w);
+return *PyType_GetSlot(&foo, Py_nb_power)(v, w, z);
+"""
+        expected_lines = """
+return ((unaryfunc)PyType_GetSlot(&foo, Py_nb_invert))(v);
+return ((binaryfunc)PyType_GetSlot(&foo, Py_nb_lshift))(v, w);
+return ((ternaryfunc)PyType_GetSlot(&foo, Py_nb_power))(v, w, z);
+"""
+        res = pcs.slot_to_macro(lines, "")
+        self.assertEqual(res, expected_lines)
+
+
+class TestTypeSlotAccess(unittest.TestCase):
+    def test_slot_to_macro(self):
+        lines = """
+Py_TYPE(foo)->tp_new
+Py_TYPE(bar)->tp_free(o)
+"""
+        expected_lines = """
+PyType_GetSlot(Py_TYPE(foo), Py_tp_new)
+((freefunc)PyType_GetSlot(Py_TYPE(bar), Py_tp_free))(o)
+"""
+        res = pcs.slot_to_macro(lines, "")
+        self.assertEqual(res, expected_lines)
+
+    def test_slot_to_ref_type(self):
+        lines = """
+PyFloat_Type.tp_alloc(v, w);
+(PyTypeObject *)PyType_Type.tp_new(v, w, x);
+"""
+        expected_lines = """
+((allocfunc)PyType_GetSlot(&PyFloat_Type, Py_tp_alloc))(v, w);
+(PyTypeObject *)((newfunc)PyType_GetSlot(&PyType_Type, Py_tp_new))(v, w, x);
+"""
+        res = pcs.slot_to_macro(lines, "")
+        self.assertEqual(res, expected_lines)
+
+    def test_slot_to_deref_callable(self):
+        lines = """
+return *PyType_GetSlot(&foo, Py_tp_free)(v);
+"""
+        expected_lines = """
+return ((freefunc)PyType_GetSlot(&foo, Py_tp_free))(v);
+"""
+        res = pcs.slot_to_macro(lines, "")
+        self.assertEqual(res, expected_lines)
+
+    def test_type_to_special_case(self):
+        lines = """
+type->tp_name
+type->tp_dict
+"""
+        expected_lines = """
+((const char*)PyObject_GetAttrString(type, "__name__"))
+PyObject_GetAttrString(type, "__dict__")
+"""
+        res = pcs.slot_to_macro(lines, "")
+        self.assertEqual(res, expected_lines)
+
+    def test_type_macro_to_special_case(self):
+        lines = """
+Py_TYPE(o)->tp_name
+Py_TYPE(o)->tp_dict
+"""
+        expected_lines = """
+((const char*)PyObject_GetAttrString(Py_TYPE(o), "__name__"))
+PyObject_GetAttrString(Py_TYPE(o), "__dict__")
+"""
+        res = pcs.slot_to_macro(lines, "")
+        self.assertEqual(res, expected_lines)
+
+
+class TestCombinedAccess(unittest.TestCase):
+    def test_four_chain_slot_to_macro(self):
+        lines = """
+res = (*v->ob_type->tp_as_sequence->sq_length)(v);
+(obj)->ob_type->tp_as_number->nb_index != NULL
+(o->ob_type->tp_as_sequence->sq_ass_item)
+"""
+        expected_lines = """
+res = (((lenfunc)PyType_GetSlot(Py_TYPE(v), Py_sq_length)))(v);
+PyType_GetSlot(Py_TYPE(obj), Py_nb_index) != NULL
+(PyType_GetSlot(Py_TYPE(o), Py_sq_ass_item))
+"""
+        res = pcs.object_to_type(lines, "")
+        res = pcs.slot_to_macro(res, "")
+        self.assertEqual(res, expected_lines)
+
+
 if __name__ == "__main__":
     unittest.main()
