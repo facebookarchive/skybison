d36856e24323d5fc324e1177d15227310325b22d
Brett Simmers <bsimmers@fb.com>
2018-11-15 18:52:00 -0800

    Default to only printing failed tests in python-tests
    This provides much more helpful output than hundreds of lines of test
    names. According to the internet this is really the best way to do this;
    googletest has no similar option built-in.
    
    Based on Facebook D10351839

--- runtime/python-test.cpp
+++ runtime/python-test.cpp
@@ -3,8 +3,89 @@
 
 extern std::string FLAGS_benchmark_filter;
 
+namespace {
+using namespace testing;
+
+// TestEventListener that ignores any events related to successful tests.
+class FailurePrinter : public EmptyTestEventListener {
+ public:
+  FailurePrinter(TestEventListener* fallback);
+  ~FailurePrinter() override;
+
+  void OnTestIterationStart(const UnitTest& test, int iteration) override;
+  void OnEnvironmentsSetUpStart(const UnitTest& test) override;
+  void OnTestPartResult(const TestPartResult& part) override;
+  void OnTestEnd(const TestInfo& test) override;
+  void OnTestCaseEnd(const TestCase& test) override;
+  void OnEnvironmentsTearDownStart(const UnitTest& test) override;
+  void OnTestIterationEnd(const UnitTest& test, int iteration) override;
+
+ private:
+  TestEventListener* fallback_;
+};
+
+FailurePrinter::FailurePrinter(TestEventListener* fallback)
+    : fallback_{fallback} {}
+
+FailurePrinter::~FailurePrinter() { delete fallback_; }
+
+void FailurePrinter::OnTestIterationStart(const UnitTest& test, int iteration) {
+  fallback_->OnTestIterationStart(test, iteration);
+}
+
+void FailurePrinter::OnEnvironmentsSetUpStart(const UnitTest& test) {
+  fallback_->OnEnvironmentsSetUpStart(test);
+}
+
+void FailurePrinter::OnTestPartResult(const TestPartResult& part) {
+  if (part.failed()) {
+    fallback_->OnTestPartResult(part);
+  }
+}
+
+void FailurePrinter::OnTestEnd(const TestInfo& test) {
+  if (test.result()->Failed()) {
+    fallback_->OnTestEnd(test);
+  }
+}
+
+void FailurePrinter::OnTestCaseEnd(const TestCase& test) {
+  if (test.Failed()) {
+    fallback_->OnTestCaseEnd(test);
+  }
+}
+
+void FailurePrinter::OnEnvironmentsTearDownStart(const UnitTest& test) {
+  fallback_->OnEnvironmentsTearDownStart(test);
+}
+
+void FailurePrinter::OnTestIterationEnd(const UnitTest& test, int iteration) {
+  fallback_->OnTestIterationEnd(test, iteration);
+}
+}  // namespace
+
 int main(int argc, char* argv[]) {
   testing::InitGoogleTest(&argc, argv);
+
+  bool failures_only = true;
+  for (int i = 0; i < argc; ++i) {
+    if (strcmp(argv[i], "--show_success") == 0) {
+      failures_only = false;
+      break;
+    }
+    if (strcmp(argv[i], "-h") == 0 || strcmp(argv[i], "--help") == 0) {
+      std::cout << "\nThis binary hides successful test names by default. To "
+                   "override this, use:\n  --show_success\n\n";
+      break;
+    }
+  }
+
+  if (failures_only) {
+    auto& listeners = testing::UnitTest::GetInstance()->listeners();
+    auto default_printer =
+        listeners.Release(listeners.default_result_printer());
+    listeners.Append(new FailurePrinter(default_printer));
+  }
 
   // Default to running no benchmarks.
   FLAGS_benchmark_filter.clear();
