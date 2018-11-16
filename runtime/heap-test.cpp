8e54b4375248041a3989456a64493569b417d9ac
Eddie Elizondo <eelizondo@fb.com>
2018-11-15 18:52:00 -0800

    [Pyro] Heap: Fix Allocate Fails test
    Summary: This tests make implicit assumptions about the free size of the heap. This changes the test so that it uses the actual free_size for allocations.
    
    Based on Facebook D7405202

--- runtime/heap-test.cpp
+++ runtime/heap-test.cpp
@@ -23,23 +23,29 @@ TEST(HeapTest, AllocateObjects) {
 }
 
 TEST(HeapTest, AllocateFails) {
-  int size = OS::kPageSize * 8;
-  Runtime runtime(size);
+  Runtime runtime;
+  HandleScope scope;
   Heap* heap = runtime.heap();
+  word free_space = heap->space()->end() - heap->space()->fill();
 
-  // Allocate the first half of the heap.
-  Object* raw1 = heap->allocate(size / 2, 0);
+  // Allocate the first half of the heap. Use a handle to prevent gc
+  word first_half = Utils::roundUp(free_space / 2, kPointerSize * 2);
+  Handle<Object> object1(&scope, heap->createLargeString(first_half));
+  Object* raw1 = *object1;
   ASSERT_NE(raw1, Error::object());
   EXPECT_TRUE(heap->contains(raw1));
 
   // Try over allocating.
-  Object* raw2 = heap->allocate(size, 0);
+  Object* raw2 = heap->allocate(free_space, 0);
   ASSERT_EQ(raw2, Error::object());
 
   // Allocate the second half of the heap.
-  Object* raw3 = heap->allocate(size / 2, 0);
+  word second_half = heap->space()->end() - heap->space()->fill();
+  Object* raw3 = heap->allocate(second_half, 0);
   ASSERT_NE(raw3, Error::object());
   EXPECT_TRUE(heap->contains(raw3));
+
+  ASSERT_EQ(heap->space()->end(), heap->space()->fill());
 }
 
 } // namespace python
