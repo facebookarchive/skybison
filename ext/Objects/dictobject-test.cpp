971d2e29a499f70ea0c04b526964a1c014030623
Eddie Elizondo <eelizondo@fb.com>
2018-11-15 18:52:00 -0800

    Convert dictobject-test to only use the C-API
    Summary: This modifies dictobject-test to move it to use the C-API only.
    
    Based on Facebook D9394900

--- ext/Objects/dictobject-test.cpp
+++ ext/Objects/dictobject-test.cpp
@@ -1,78 +1,43 @@
 #include "gtest/gtest.h"
 
 #include "Python.h"
-#include "runtime.h"
-#include "test-utils.h"
+#include "capi-fixture.h"
+#include "capi-testing.h"
 
 namespace python {
 
-TEST(DictObject, GetItemFromNonDictReturnsNull) {
-  Runtime runtime;
-  HandleScope scope;
+using DictExtensionApiTest = ExtensionApi;
 
-  Handle<Object> non_dict(&scope, runtime.newInt(10));
-
-  // Pass a non dict
-  PyObject* result = PyDict_GetItem(ApiHandle::fromObject(*non_dict),
-                                    ApiHandle::fromObject(*non_dict));
+TEST_F(DictExtensionApiTest, GetItemFromNonDictionaryReturnsNull) {
+  // Pass a non dictionary
+  PyObject* result = PyDict_GetItem(Py_None, Py_None);
   EXPECT_EQ(result, nullptr);
 }
 
-TEST(DictObject, GetItemNonExistingKeyReturnsNull) {
-  Runtime runtime;
-  HandleScope scope;
-
-  Handle<Dict> dict(&scope, runtime.newDict());
-  Handle<Object> nonkey(&scope, SmallInt::fromWord(30));
+TEST_F(DictExtensionApiTest, GetItemNonExistingKeyReturnsNull) {
+  PyObject* dict = PyDict_New();
+  PyObject* nonkey = PyLong_FromLong(10);
 
   // Pass a non existing key
-  PyObject* result = PyDict_GetItem(ApiHandle::fromObject(*dict),
-                                    ApiHandle::fromObject(*nonkey));
+  PyObject* result = PyDict_GetItem(dict, nonkey);
   EXPECT_EQ(result, nullptr);
 }
 
-TEST(DictObject, GetItemReturnsValue) {
-  Runtime runtime;
-  HandleScope scope;
+TEST_F(DictExtensionApiTest, GetItemReturnsBorrowedValue) {
+  PyObject* dict = PyDict_New();
+  PyObject* key = PyLong_FromLong(10);
+  PyObject* value = PyLong_FromLong(20);
 
-  Handle<Dict> dict(&scope, runtime.newDict());
-  Handle<Object> key(&scope, runtime.newInt(10));
-  Handle<Object> value(&scope, runtime.newInt(20));
-  runtime.dictAtPut(dict, key, value);
+  // Insert key value
+  ASSERT_EQ(PyDict_SetItem(dict, key, value), 0);
 
   // Pass existing key
-  PyObject* result =
-      PyDict_GetItem(ApiHandle::fromObject(*dict), ApiHandle::fromObject(*key));
+  PyObject* result = PyDict_GetItem(dict, key);
   ASSERT_NE(result, nullptr);
 
   // Check result value
-  Handle<Object> result_obj(&scope,
-                            ApiHandle::fromPyObject(result)->asObject());
-  ASSERT_TRUE(result_obj->isInt());
-  EXPECT_EQ(Int::cast(*result_obj)->asWord(), 20);
-}
-
-TEST(DictObject, GetItemReturnsBorrowedReference) {
-  Runtime runtime;
-  HandleScope scope;
-
-  Handle<Dict> dict(&scope, runtime.newDict());
-  Handle<Object> key(&scope, runtime.newInt(10));
-  Handle<Object> value(&scope, runtime.newInt(20));
-  runtime.dictAtPut(dict, key, value);
-
-  // Pass existing key
-  PyObject* result =
-      PyDict_GetItem(ApiHandle::fromObject(*dict), ApiHandle::fromObject(*key));
-  ASSERT_NE(result, nullptr);
-
-  // Check that key is a normal handle
-  ApiHandle* key_handle = ApiHandle::fromObject(*key);
-  EXPECT_FALSE(key_handle->isBorrowed());
-
-  // Check that value returned PyDict_GetItem is a borrowed reference
-  ApiHandle* value_handle = ApiHandle::fromObject(*value);
-  EXPECT_TRUE(value_handle->isBorrowed());
+  EXPECT_EQ(PyLong_AsLong(result), 20);
+  EXPECT_TRUE(testing::isBorrowed(result));
 }
 
 }  // namespace python
