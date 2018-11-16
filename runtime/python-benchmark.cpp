a8db70bf4ebbb3b791f8da851bcdb208d2951b8f
Eddie Elizondo <eelizondo@fb.com>
2018-11-15 18:52:00 -0800

    [Pyro] Split Tests and Benchmarks
    Split tests and benchmarks to run independently from each other.
    
    This adopts a standard of writing tests and benchmarks under:
    *-test.cpp
    *-benchmark.cpp
    
    Based on Facebook D7384929

---
+++ runtime/python-benchmark.cpp
@@ -0,0 +1,7 @@
+#include "benchmark/benchmark.h"
+
+int main(int argc, char* argv[]) {
+  benchmark::Initialize(&argc, argv);
+  benchmark::RunSpecifiedBenchmarks();
+  return 0;
+}
--- runtime/python-test.cpp
+++ runtime/python-test.cpp
@@ -1,13 +1,10 @@
-#include "benchmark/benchmark.h"
 #include "gtest/gtest.h"
 
 int main(int argc, char* argv[]) {
   testing::InitGoogleTest(&argc, argv);
-  benchmark::Initialize(&argc, argv);
   auto status = RUN_ALL_TESTS();
   if (status != 0) {
     return status;
   }
-  benchmark::RunSpecifiedBenchmarks();
   return 0;
 }
