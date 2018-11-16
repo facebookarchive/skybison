8ba63a836aa4b72f6ae714fd529e894c3f9c5928
Eddie Elizondo <eelizondo@fb.com>
2018-11-15 18:52:00 -0800

    [Pyro] Extensions: Partial implemnentation of PyUnicode_AsUT8AndSize
    Add in a partial implementation of PyUnicode_AsUT8AndSize. This is still missing the caching functionality. Meaning that all generated CStrings from this function will be newly allocated.
    
    Currently, this function is required to be able to link in some of the unmodified functions required for PyType_Ready. None of them require the identity functionality so it's fine to push this feature to be implemented later.
    
    This change also adds a modification to the String::toCString() api, adding a length component to only set the specified number of chars in the newly allocated CString. This defaults to the full string.
    
    Based on Facebook D8127516

--- ext/Objects/unicodeobject-test.cpp
+++ ext/Objects/unicodeobject-test.cpp
@@ -18,4 +18,31 @@ TEST(ExtensionTest, _PyUnicode_FromId) {
   EXPECT_TRUE(String::cast(*string_obj)->equalsCString(str));
 }
 
+TEST(ExtensionTest, PyUnicode_AsUTF8AndSize) {
+  Runtime runtime;
+  HandleScope scope;
+
+  // Pass a non string object
+  Handle<Object> integer_obj(&scope, runtime.newInteger(15));
+  PyObject* pylong = runtime.asPyObject(*integer_obj);
+  char* null_cstring2 = PyUnicode_AsUTF8AndSize(pylong, nullptr);
+  EXPECT_EQ(nullptr, null_cstring2);
+
+  const char* str = "Some C String";
+  Handle<String> string_obj(&scope, runtime.newStringFromCString(str));
+  PyObject* pyunicode = runtime.asPyObject(*string_obj);
+
+  // Pass a nullptr size
+  char* cstring = PyUnicode_AsUTF8AndSize(pyunicode, nullptr);
+  EXPECT_STREQ(str, cstring);
+  std::free(cstring);
+
+  // Pass a size reference
+  Py_ssize_t size = 0;
+  char* cstring2 = PyUnicode_AsUTF8AndSize(pyunicode, &size);
+  EXPECT_STREQ(str, cstring2);
+  EXPECT_EQ(size, strlen(str));
+  std::free(cstring2);
+}
+
 } // namespace python
--- ext/Objects/unicodeobject.cpp
+++ ext/Objects/unicodeobject.cpp
@@ -26,3 +26,29 @@ PyObject* _PyUnicode_FromId(_Py_Identifier* id) {
       &scope, runtime->internStringFromCString(id->string));
   return runtime->asPyObject(*result);
 }
+
+char* PyUnicode_AsUTF8AndSize(PyObject* pyunicode, Py_ssize_t* size) {
+  py::Thread* thread = py::Thread::currentThread();
+  py::Runtime* runtime = thread->runtime();
+  py::HandleScope scope(thread->handles());
+
+  if (pyunicode == nullptr) {
+    PyErr_BadArgument();
+    return nullptr;
+  }
+
+  py::Handle<py::Object> str_obj(&scope, runtime->asObject(pyunicode));
+  if (!str_obj->isString()) {
+    return nullptr;
+  }
+
+  py::Handle<py::String> str(&scope, *str_obj);
+  word length = str->length();
+  byte* result = static_cast<byte*>(std::malloc(length + 1));
+  str->copyTo(result, length);
+  result[length] = '\0';
+  if (size) {
+    *size = length;
+  }
+  return reinterpret_cast<char*>(result);
+}
