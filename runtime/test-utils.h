503afb1e178e5427fd25e9df3b84608ea76c1cfa
Mark Logan <mlogan@fb.com>
2018-11-16 14:58:26 -0800

    Add EXPECT_DEBUG_ONLY_DEATH macro
    Add EXPECT_DEBUG_ONLY_DEATH macro
    
    gtest still evaluates the expression in EXPECT_DEBUG_DEATH even
    in release mode, it just doesn't require the process to exit..
    This means that unsafe operations can be executed in release test
    builds, which can cause the asan tests to fail. (They also cause
    undefined behavior in non-asan release tests.)
    
    Based on Facebook D6631492

--- runtime/test-utils.h
+++ runtime/test-utils.h
@@ -1,6 +1,23 @@
 #pragma once
 #include <string>
 
+// Define EXPECT_DEBUG_ONLY_DEATH, which is like gtest's
+// EXPECT_DEBUG_DEATH, except that it does not execute the given statment
+// at all in release mode. This is necessary because most of our tests
+// which rely on EXPECT_DEBUG_DEATH will cause unsafe operations to
+// occur if the statement is executed.
+
+#ifdef NDEBUG
+
+#define EXPECT_DEBUG_ONLY_DEATH(stmt, pattern)
+
+#else
+
+#define EXPECT_DEBUG_ONLY_DEATH(stmt, pattern) \
+  EXPECT_DEBUG_DEATH((stmt), (pattern))
+
+#endif
+
 namespace python {
 class Runtime;
 namespace testing {
