d9a4c806ed3ff71760a597ecb99831d274b090f9
William Meehan <wmeehan@fb.com>
2018-11-16 14:58:48 -0800

    Cleanup abstract.cpp function order
    Summary: Keep functions on the same PyObject type together, and alphabetize by that type
    
    Based on Facebook D12944377

--- ext/Objects/abstract.cpp
+++ ext/Objects/abstract.cpp
@@ -17,12 +17,50 @@ PY_EXPORT void PyBuffer_Release(Py_buffer* /* w */) {
   UNIMPLEMENTED("PyBuffer_Release");
 }
 
+PY_EXPORT PyObject* PyIter_Next(PyObject* /* r */) {
+  UNIMPLEMENTED("PyIter_Next");
+}
+
 PY_EXPORT int PyMapping_Check(PyObject* /* o */) {
   UNIMPLEMENTED("PyMapping_Check");
 }
 
+PY_EXPORT PyObject* PyMapping_GetItemString(PyObject* /* o */,
+                                            const char* /* y */) {
+  UNIMPLEMENTED("PyMapping_GetItemString");
+}
+
+PY_EXPORT int PyMapping_HasKey(PyObject* /* o */, PyObject* /* y */) {
+  UNIMPLEMENTED("PyMapping_HasKey");
+}
+
+PY_EXPORT int PyMapping_HasKeyString(PyObject* /* o */, const char* /* y */) {
+  UNIMPLEMENTED("PyMapping_HasKeyString");
+}
+
+PY_EXPORT PyObject* PyMapping_Items(PyObject* /* o */) {
+  UNIMPLEMENTED("PyMapping_Items");
+}
+
+PY_EXPORT PyObject* PyMapping_Keys(PyObject* /* o */) {
+  UNIMPLEMENTED("PyMapping_Keys");
+}
+
+PY_EXPORT Py_ssize_t PyMapping_Length(PyObject* /* o */) {
+  UNIMPLEMENTED("PyMapping_Length");
+}
+
+PY_EXPORT int PyMapping_SetItemString(PyObject* /* o */, const char* /* y */,
+                                      PyObject* /* e */) {
+  UNIMPLEMENTED("PyMapping_SetItemString");
+}
+
 PY_EXPORT Py_ssize_t PyMapping_Size(PyObject* /* o */) {
   UNIMPLEMENTED("PyMapping_Size");
+}
+
+PY_EXPORT PyObject* PyMapping_Values(PyObject* /* o */) {
+  UNIMPLEMENTED("PyMapping_Values");
 }
 
 PY_EXPORT PyObject* PyNumber_Absolute(PyObject* /* o */) {
@@ -240,88 +278,6 @@ PY_EXPORT Py_ssize_t PyObject_Size(PyObject* /* o */) {
   UNIMPLEMENTED("PyObject_Size");
 }
 
-PY_EXPORT int PySequence_Check(PyObject* /* s */) {
-  UNIMPLEMENTED("PySequence_Check");
-}
-
-PY_EXPORT PyObject* PySequence_Concat(PyObject* /* s */, PyObject* /* o */) {
-  UNIMPLEMENTED("PySequence_Concat");
-}
-
-PY_EXPORT int PySequence_Contains(PyObject* /* q */, PyObject* /* b */) {
-  UNIMPLEMENTED("PySequence_Contains");
-}
-
-PY_EXPORT PyObject* PySequence_GetItem(PyObject* /* s */, Py_ssize_t /* i */) {
-  UNIMPLEMENTED("PySequence_GetItem");
-}
-
-PY_EXPORT PyObject* PySequence_GetSlice(PyObject* /* s */, Py_ssize_t /* 1 */,
-                                        Py_ssize_t /* 2 */) {
-  UNIMPLEMENTED("PySequence_GetSlice");
-}
-
-PY_EXPORT PyObject* PySequence_InPlaceConcat(PyObject* /* s */,
-                                             PyObject* /* o */) {
-  UNIMPLEMENTED("PySequence_InPlaceConcat");
-}
-
-PY_EXPORT PyObject* PySequence_InPlaceRepeat(PyObject* /* o */,
-                                             Py_ssize_t /* t */) {
-  UNIMPLEMENTED("PySequence_InPlaceRepeat");
-}
-
-PY_EXPORT PyObject* PySequence_Repeat(PyObject* /* o */, Py_ssize_t /* t */) {
-  UNIMPLEMENTED("PySequence_Repeat");
-}
-
-PY_EXPORT int PySequence_SetItem(PyObject* /* s */, Py_ssize_t /* i */,
-                                 PyObject* /* o */) {
-  UNIMPLEMENTED("PySequence_SetItem");
-}
-
-PY_EXPORT Py_ssize_t PySequence_Size(PyObject* /* s */) {
-  UNIMPLEMENTED("PySequence_Size");
-}
-
-PY_EXPORT PyObject* PyIter_Next(PyObject* /* r */) {
-  UNIMPLEMENTED("PyIter_Next");
-}
-
-PY_EXPORT PyObject* PyMapping_GetItemString(PyObject* /* o */,
-                                            const char* /* y */) {
-  UNIMPLEMENTED("PyMapping_GetItemString");
-}
-
-PY_EXPORT int PyMapping_HasKey(PyObject* /* o */, PyObject* /* y */) {
-  UNIMPLEMENTED("PyMapping_HasKey");
-}
-
-PY_EXPORT int PyMapping_HasKeyString(PyObject* /* o */, const char* /* y */) {
-  UNIMPLEMENTED("PyMapping_HasKeyString");
-}
-
-PY_EXPORT PyObject* PyMapping_Items(PyObject* /* o */) {
-  UNIMPLEMENTED("PyMapping_Items");
-}
-
-PY_EXPORT PyObject* PyMapping_Keys(PyObject* /* o */) {
-  UNIMPLEMENTED("PyMapping_Keys");
-}
-
-PY_EXPORT Py_ssize_t PyMapping_Length(PyObject* /* o */) {
-  UNIMPLEMENTED("PyMapping_Length");
-}
-
-PY_EXPORT int PyMapping_SetItemString(PyObject* /* o */, const char* /* y */,
-                                      PyObject* /* e */) {
-  UNIMPLEMENTED("PyMapping_SetItemString");
-}
-
-PY_EXPORT PyObject* PyMapping_Values(PyObject* /* o */) {
-  UNIMPLEMENTED("PyMapping_Values");
-}
-
 PY_EXPORT Py_ssize_t PyNumber_AsSsize_t(PyObject* /* m */, PyObject* /* r */) {
   UNIMPLEMENTED("PyNumber_AsSsize_t");
 }
@@ -372,12 +328,65 @@ PY_EXPORT int PyObject_AsReadBuffer(PyObject* /* j */,
   UNIMPLEMENTED("PyObject_AsReadBuffer");
 }
 
+PY_EXPORT PyObject* PyObject_Call(PyObject* /* e */, PyObject* /* s */,
+                                  PyObject* /* s */) {
+  UNIMPLEMENTED("PyObject_Call");
+}
+
+PY_EXPORT PyObject* PyObject_CallFunction(PyObject* /* e */,
+                                          const char* /* t */, ...) {
+  UNIMPLEMENTED("PyObject_CallFunction");
+}
+
+PY_EXPORT PyObject* PyObject_CallFunctionObjArgs(PyObject* /* e */, ...) {
+  UNIMPLEMENTED("PyObject_CallFunctionObjArgs");
+}
+
+PY_EXPORT PyObject* _PyObject_CallFunction_SizeT(PyObject* /* e */,
+                                                 const char* /* t */, ...) {
+  UNIMPLEMENTED("_PyObject_CallFunction_SizeT");
+}
+
+PY_EXPORT PyObject* PyObject_CallMethod(PyObject* /* j */, const char* /* e */,
+                                        const char* /* t */, ...) {
+  UNIMPLEMENTED("PyObject_CallMethod");
+}
+
+PY_EXPORT PyObject* PyObject_CallMethodObjArgs(PyObject* /* e */,
+                                               PyObject* /* e */, ...) {
+  UNIMPLEMENTED("PyObject_CallMethodObjArgs");
+}
+
+PY_EXPORT PyObject* _PyObject_CallMethod_SizeT(PyObject* /* j */,
+                                               const char* /* e */,
+                                               const char* /* t */, ...) {
+  UNIMPLEMENTED("_PyObject_CallMethod_SizeT");
+}
+
+PY_EXPORT PyObject* PyObject_CallObject(PyObject* /* e */, PyObject* /* s */) {
+  UNIMPLEMENTED("PyObject_CallObject");
+}
+
 PY_EXPORT int PyObject_CheckReadBuffer(PyObject* /* j */) {
   UNIMPLEMENTED("PyObject_CheckReadBuffer");
 }
 
 PY_EXPORT int PyObject_DelItemString(PyObject* /* o */, const char* /* y */) {
   UNIMPLEMENTED("PyObject_DelItemString");
+}
+
+PY_EXPORT PyObject* _PyObject_FastCallDict(PyObject* /* e */,
+                                           PyObject* const* /* s */,
+                                           Py_ssize_t /* s */,
+                                           PyObject* /* s */) {
+  UNIMPLEMENTED("_PyObject_FastCallDict");
+}
+
+PY_EXPORT PyObject* _PyObject_FastCallKeywords(PyObject* /* e */,
+                                               PyObject* const* /* k */,
+                                               Py_ssize_t /* s */,
+                                               PyObject* /* s */) {
+  UNIMPLEMENTED("_PyObject_FastCallKeywords");
 }
 
 PY_EXPORT PyObject* PyObject_Format(PyObject* /* j */, PyObject* /* c */) {
@@ -400,8 +409,25 @@ PY_EXPORT Py_ssize_t PyObject_Length(PyObject* /* o */) {
   UNIMPLEMENTED("PyObject_Length");
 }
 
+PY_EXPORT Py_ssize_t PyObject_LengthHint(PyObject* /* o */,
+                                         Py_ssize_t /* defaultvalue */) {
+  UNIMPLEMENTED("PyObject_LengthHint");
+}
+
 PY_EXPORT PyObject* PyObject_Type(PyObject* /* o */) {
   UNIMPLEMENTED("PyObject_Type");
+}
+
+PY_EXPORT int PySequence_Check(PyObject* /* s */) {
+  UNIMPLEMENTED("PySequence_Check");
+}
+
+PY_EXPORT PyObject* PySequence_Concat(PyObject* /* s */, PyObject* /* o */) {
+  UNIMPLEMENTED("PySequence_Concat");
+}
+
+PY_EXPORT int PySequence_Contains(PyObject* /* q */, PyObject* /* b */) {
+  UNIMPLEMENTED("PySequence_Contains");
 }
 
 PY_EXPORT Py_ssize_t PySequence_Count(PyObject* /* s */, PyObject* /* o */) {
@@ -421,12 +447,31 @@ PY_EXPORT PyObject* PySequence_Fast(PyObject* /* v */, const char* /* m */) {
   UNIMPLEMENTED("PySequence_Fast");
 }
 
+PY_EXPORT PyObject* PySequence_GetItem(PyObject* /* s */, Py_ssize_t /* i */) {
+  UNIMPLEMENTED("PySequence_GetItem");
+}
+
+PY_EXPORT PyObject* PySequence_GetSlice(PyObject* /* s */, Py_ssize_t /* 1 */,
+                                        Py_ssize_t /* 2 */) {
+  UNIMPLEMENTED("PySequence_GetSlice");
+}
+
 PY_EXPORT int PySequence_In(PyObject* /* w */, PyObject* /* v */) {
   UNIMPLEMENTED("PySequence_In");
 }
 
 PY_EXPORT Py_ssize_t PySequence_Index(PyObject* /* s */, PyObject* /* o */) {
   UNIMPLEMENTED("PySequence_Index");
+}
+
+PY_EXPORT PyObject* PySequence_InPlaceConcat(PyObject* /* s */,
+                                             PyObject* /* o */) {
+  UNIMPLEMENTED("PySequence_InPlaceConcat");
+}
+
+PY_EXPORT PyObject* PySequence_InPlaceRepeat(PyObject* /* o */,
+                                             Py_ssize_t /* t */) {
+  UNIMPLEMENTED("PySequence_InPlaceRepeat");
 }
 
 PY_EXPORT Py_ssize_t PySequence_Length(PyObject* /* s */) {
@@ -437,71 +482,26 @@ PY_EXPORT PyObject* PySequence_List(PyObject* /* v */) {
   UNIMPLEMENTED("PySequence_List");
 }
 
+PY_EXPORT PyObject* PySequence_Repeat(PyObject* /* o */, Py_ssize_t /* t */) {
+  UNIMPLEMENTED("PySequence_Repeat");
+}
+
+PY_EXPORT int PySequence_SetItem(PyObject* /* s */, Py_ssize_t /* i */,
+                                 PyObject* /* o */) {
+  UNIMPLEMENTED("PySequence_SetItem");
+}
+
 PY_EXPORT int PySequence_SetSlice(PyObject* /* s */, Py_ssize_t /* 1 */,
                                   Py_ssize_t /* 2 */, PyObject* /* o */) {
   UNIMPLEMENTED("PySequence_SetSlice");
 }
 
+PY_EXPORT Py_ssize_t PySequence_Size(PyObject* /* s */) {
+  UNIMPLEMENTED("PySequence_Size");
+}
+
 PY_EXPORT PyObject* PySequence_Tuple(PyObject* /* v */) {
   UNIMPLEMENTED("PySequence_Tuple");
-}
-
-PY_EXPORT PyObject* PyObject_CallObject(PyObject* /* e */, PyObject* /* s */) {
-  UNIMPLEMENTED("PyObject_CallObject");
-}
-
-PY_EXPORT PyObject* PyObject_CallFunction(PyObject* /* e */,
-                                          const char* /* t */, ...) {
-  UNIMPLEMENTED("PyObject_CallFunction");
-}
-
-PY_EXPORT PyObject* PyObject_CallFunctionObjArgs(PyObject* /* e */, ...) {
-  UNIMPLEMENTED("PyObject_CallFunctionObjArgs");
-}
-
-PY_EXPORT PyObject* PyObject_CallMethod(PyObject* /* j */, const char* /* e */,
-                                        const char* /* t */, ...) {
-  UNIMPLEMENTED("PyObject_CallMethod");
-}
-
-PY_EXPORT PyObject* PyObject_CallMethodObjArgs(PyObject* /* e */,
-                                               PyObject* /* e */, ...) {
-  UNIMPLEMENTED("PyObject_CallMethodObjArgs");
-}
-
-PY_EXPORT PyObject* PyObject_Call(PyObject* /* e */, PyObject* /* s */,
-                                  PyObject* /* s */) {
-  UNIMPLEMENTED("PyObject_Call");
-}
-
-PY_EXPORT PyObject* _PyObject_CallFunction_SizeT(PyObject* /* e */,
-                                                 const char* /* t */, ...) {
-  UNIMPLEMENTED("_PyObject_CallFunction_SizeT");
-}
-
-PY_EXPORT PyObject* _PyObject_CallMethod_SizeT(PyObject* /* j */,
-                                               const char* /* e */,
-                                               const char* /* t */, ...) {
-  UNIMPLEMENTED("_PyObject_CallMethod_SizeT");
-}
-
-PY_EXPORT PyObject* _PyObject_FastCallDict(PyObject* /* e */,
-                                           PyObject* const* /* s */,
-                                           Py_ssize_t /* s */,
-                                           PyObject* /* s */) {
-  UNIMPLEMENTED("_PyObject_FastCallDict");
-}
-
-PY_EXPORT PyObject* _PyObject_FastCallKeywords(PyObject* /* e */,
-                                               PyObject* const* /* k */,
-                                               Py_ssize_t /* s */,
-                                               PyObject* /* s */) {
-  UNIMPLEMENTED("_PyObject_FastCallKeywords");
-}
-
-PY_EXPORT Py_ssize_t PyObject_LengthHint(PyObject* /* o */,
-                                         Py_ssize_t /* defaultvalue */) {
-  UNIMPLEMENTED("PyObject_LengthHint");
 }
 
 }  // namespace python
