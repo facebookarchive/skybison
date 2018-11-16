70d41787024d2f7db7eb1a9c0a1fcecffb288a16
Eddie Elizondo <eelizondo@fb.com>
2018-11-15 18:52:00 -0800

    [Pyro] C-API: Convert unicodeobject-test to only use the C-API
    Summary: Modify unicodeobject-test to only use the C-API.
    
    Based on Facebook D9393912

--- ext/Objects/unicodeobject-test.cpp
+++ ext/Objects/unicodeobject-test.cpp
@@ -1,30 +1,18 @@
-#include "gtest/gtest.h"
-
-#include "Python.h"
-#include "runtime.h"
-#include "test-utils.h"
+#include "capi-fixture.h"
 
 namespace python {
 
-TEST(UnicodeObject, AsUTF8FromNonStringReturnsNull) {
-  Runtime runtime;
-  HandleScope scope;
+using UnicodeExtensionApiTest = ExtensionApi;
 
-  Handle<Object> integer_obj(&scope, runtime.newInt(15));
-  PyObject* pylong = ApiHandle::fromObject(*integer_obj)->asPyObject();
-
+TEST_F(UnicodeExtensionApiTest, AsUTF8FromNonStringReturnsNull) {
   // Pass a non string object
-  char* cstring = PyUnicode_AsUTF8AndSize(pylong, nullptr);
+  char* cstring = PyUnicode_AsUTF8AndSize(Py_None, nullptr);
   EXPECT_EQ(nullptr, cstring);
 }
 
-TEST(UnicodeObject, AsUTF8WithNullSizeReturnsCString) {
-  Runtime runtime;
-  HandleScope scope;
-
+TEST_F(UnicodeExtensionApiTest, AsUTF8WithNullSizeReturnsCString) {
   const char* str = "Some C String";
-  Handle<String> string_obj(&scope, runtime.newStringFromCString(str));
-  PyObject* pyunicode = ApiHandle::fromObject(*string_obj)->asPyObject();
+  PyObject* pyunicode = PyUnicode_FromString(str);
 
   // Pass a nullptr size
   char* cstring = PyUnicode_AsUTF8AndSize(pyunicode, nullptr);
@@ -33,13 +21,9 @@ TEST(UnicodeObject, AsUTF8WithNullSizeReturnsCString) {
   std::free(cstring);
 }
 
-TEST(UnicodeObject, AsUTF8WithReferencedSizeReturnsCString) {
-  Runtime runtime;
-  HandleScope scope;
-
+TEST_F(UnicodeExtensionApiTest, AsUTF8WithReferencedSizeReturnsCString) {
   const char* str = "Some C String";
-  Handle<String> string_obj(&scope, runtime.newStringFromCString(str));
-  PyObject* pyunicode = ApiHandle::fromObject(*string_obj)->asPyObject();
+  PyObject* pyunicode = PyUnicode_FromString(str);
 
   // Pass a size reference
   Py_ssize_t size = 0;
