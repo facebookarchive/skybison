1f3f75472f06cd96e1cdba606f73aad3eaa7ed91
Raymond Ng <rkng@fb.com>
2018-11-15 18:52:00 -0800

    trampolines: use snake_case for args
    Summary: Trampolines: use snake_case for args
    
    Based on Facebook D8806115

--- runtime/trampolines.h
+++ runtime/trampolines.h
@@ -11,34 +11,34 @@ class Function;
 class Code;
 
 // Entry points for interpreted functions
-Object* interpreterTrampoline(Thread* thread, Frame* callerFrame, word argc)
+Object* interpreterTrampoline(Thread* thread, Frame* caller_frame, word argc)
     __attribute__((aligned(16)));
 Object* interpreterTrampolineSlowPath(Thread* thread, Function* function,
-                                      Code* code, Frame* callerFrame, word argc)
+                                      Code* code, Frame* caller_frame,
+                                      word argc) __attribute__((aligned(16)));
+Object* interpreterTrampolineKw(Thread* thread, Frame* caller_frame, word argc)
     __attribute__((aligned(16)));
-Object* interpreterTrampolineKw(Thread* thread, Frame* callerFrame, word argc)
-    __attribute__((aligned(16)));
-Object* interpreterTrampolineEx(Thread* thread, Frame* callerFrame, word argc)
+Object* interpreterTrampolineEx(Thread* thread, Frame* caller_frame, word argc)
     __attribute__((aligned(16)));
 
 // Aborts immediately when called
-Object* unimplementedTrampoline(Thread* thread, Frame* callerFrame, word argc)
+Object* unimplementedTrampoline(Thread* thread, Frame* caller_frame, word argc)
     __attribute__((aligned(16)));
 
 // Force 16-byte alignment on trampoline addresses to disguise them as
 // smallIntegers to avoid GC issues
 template <Object* (*Fn)(Thread*, Frame*, word)>
-Object* nativeTrampoline(Thread* thread, Frame* callerFrame, word argc)
+Object* nativeTrampoline(Thread* thread, Frame* caller_frame, word argc)
     __attribute__((aligned(16)));
 template <Object* (*Fn)(Thread*, Frame*, word)>
-Object* nativeTrampolineKw(Thread* thread, Frame* callerFrame, word argc)
+Object* nativeTrampolineKw(Thread* thread, Frame* caller_frame, word argc)
     __attribute__((aligned(16)));
 
-Object* extensionTrampoline(Thread* thread, Frame* previousFrame, word argc)
+Object* extensionTrampoline(Thread* thread, Frame* previous_frame, word argc)
     __attribute__((aligned(16)));
-Object* extensionTrampolineKw(Thread* thread, Frame* previousFrame, word argc)
+Object* extensionTrampolineKw(Thread* thread, Frame* previous_frame, word argc)
     __attribute__((aligned(16)));
-Object* extensionTrampolineEx(Thread* thread, Frame* previousFrame, word argc)
+Object* extensionTrampolineEx(Thread* thread, Frame* previous_frame, word argc)
     __attribute__((aligned(16)));
 
 }  // namespace python
