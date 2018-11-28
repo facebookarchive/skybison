65bb541bea425be75d20630dc05a9c79fb5b9ec4
William Meehan <wmeehan@fb.com>
2018-11-28 10:34:14 -0800

    Implement PyBytes_FromString(AndSize)
    Summary: Make the function for creating Bytes from C strings.
    
    Based on Facebook D13083930

---
+++ ext/Objects/bytesobject-test.cpp
@@ -0,0 +1,74 @@
+#include "gtest/gtest.h"
+
+#include "Python.h"
+#include "capi-fixture.h"
+#include "capi-testing.h"
+
+namespace python {
+
+using namespace testing;
+
+using BytesExtensionApiTest = ExtensionApi;
+
+TEST_F(BytesExtensionApiTest, FromStringAndSizeIncrementsRefCount) {
+  PyObject* bytes = PyBytes_FromStringAndSize("foo", 3);
+  ASSERT_NE(bytes, nullptr);
+  EXPECT_GE(Py_REFCNT(bytes), 1);
+  Py_DECREF(bytes);
+  EXPECT_EQ(PyErr_Occurred(), nullptr);
+}
+
+TEST_F(BytesExtensionApiTest, FromStringAndSizeReturnsBytes) {
+  PyObjectPtr bytes(PyBytes_FromStringAndSize("foo", 3));
+  EXPECT_TRUE(PyBytes_CheckExact(bytes));
+  // TODO(T35906055): EXPECT_EQ(PyBytes_Size(bytes), 3);
+}
+
+TEST_F(BytesExtensionApiTest,
+       FromStringAndSizeWithEmptyStringReturnsEmptyBytes) {
+  PyObjectPtr bytes(PyBytes_FromStringAndSize("", 0));
+  EXPECT_TRUE(PyBytes_CheckExact(bytes));
+  // TODO(T35906055): EXPECT_EQ(PyBytes_Size(bytes), 0);
+}
+
+TEST_F(BytesExtensionApiTest, FromStringAndSizeWithNegativeSizeReturnsNull) {
+  EXPECT_EQ(PyBytes_FromStringAndSize("foo", -1), nullptr);
+  EXPECT_NE(PyErr_Occurred(), nullptr);
+  // TODO(miro): replace with PyErr_ExceptionMatches(PyExc_SystemError);
+  const char* expected = "Negative size passed to PyBytes_FromStringAndSize";
+  EXPECT_TRUE(testing::exceptionValueMatches(expected));
+}
+
+TEST_F(BytesExtensionApiTest, FromStringAndSizeWithShorterSize) {
+  PyObjectPtr bytes(PyBytes_FromStringAndSize("foo bar", 5));
+  EXPECT_TRUE(PyBytes_CheckExact(bytes));
+  // TODO(T35906055): EXPECT_EQ(PyBytes_Size(bytes), 5);
+}
+
+TEST_F(BytesExtensionApiTest, FromStringAndSizeWithSizeZero) {
+  PyObjectPtr bytes(PyBytes_FromStringAndSize("foo bar", 0));
+  EXPECT_TRUE(PyBytes_CheckExact(bytes));
+  // TODO(T35906055): EXPECT_EQ(PyBytes_Size(bytes), 0);
+}
+
+TEST_F(BytesExtensionApiTest, FromStringIncrementsRefCount) {
+  PyObject* bytes = PyBytes_FromString("foo");
+  ASSERT_NE(bytes, nullptr);
+  EXPECT_GE(Py_REFCNT(bytes), 1);
+  Py_DECREF(bytes);
+  EXPECT_EQ(PyErr_Occurred(), nullptr);
+}
+
+TEST_F(BytesExtensionApiTest, FromStringReturnsBytes) {
+  PyObjectPtr bytes(PyBytes_FromString("foo"));
+  EXPECT_TRUE(PyBytes_CheckExact(bytes));
+  // TODO(T35906055): EXPECT_EQ(PyBytes_Size(bytes), 3);
+}
+
+TEST_F(BytesExtensionApiTest, FromStringWithEmptyStringReturnsEmptyBytes) {
+  PyObjectPtr bytes(PyBytes_FromString(""));
+  EXPECT_TRUE(PyBytes_CheckExact(bytes));
+  // TODO(T35906055): EXPECT_EQ(PyBytes_Size(bytes), 0);
+}
+
+}  // namespace python
--- ext/Objects/bytesobject.cpp
+++ ext/Objects/bytesobject.cpp
@@ -18,15 +18,6 @@ PY_EXPORT char* PyBytes_AsString(PyObject* /* p */) {
   UNIMPLEMENTED("PyBytes_AsString");
 }
 
-PY_EXPORT PyObject* PyBytes_FromString(const char* /* r */) {
-  UNIMPLEMENTED("PyBytes_FromString");
-}
-
-PY_EXPORT PyObject* PyBytes_FromStringAndSize(const char* /* r */,
-                                              Py_ssize_t /* e */) {
-  UNIMPLEMENTED("PyBytes_FromStringAndSize");
-}
-
 PY_EXPORT int PyBytes_AsStringAndSize(PyObject* /* j */, char** /* s */,
                                       Py_ssize_t* /* n */) {
   UNIMPLEMENTED("PyBytes_AsStringAndSize");
@@ -58,6 +49,44 @@ PY_EXPORT PyObject* PyBytes_FromFormatV(const char* /* t */, va_list /* s */) {
 
 PY_EXPORT PyObject* PyBytes_FromObject(PyObject* /* x */) {
   UNIMPLEMENTED("PyBytes_FromObject");
+}
+
+PY_EXPORT PyObject* PyBytes_FromStringAndSize(const char* str,
+                                              Py_ssize_t size) {
+  Thread* thread = Thread::currentThread();
+
+  if (size < 0) {
+    thread->raiseSystemErrorWithCStr(
+        "Negative size passed to PyBytes_FromStringAndSize");
+    return nullptr;
+  }
+
+  if (str == nullptr) {
+    UNIMPLEMENTED("mutable, uninitialized bytes");
+  }
+
+  // TODO(T36997048): intern 1-element byte arrays
+
+  Runtime* runtime = thread->runtime();
+  HandleScope scope(thread);
+
+  View<byte> view(reinterpret_cast<const byte*>(str), size);
+  Object bytes_obj(&scope, runtime->newBytesWithAll(view));
+
+  return ApiHandle::fromObject(*bytes_obj);
+}
+
+PY_EXPORT PyObject* PyBytes_FromString(const char* str) {
+  DCHECK(str, "nullptr argument");
+  Thread* thread = Thread::currentThread();
+
+  uword size = strlen(str);
+  if (size > kMaxWord) {
+    thread->raiseOverflowErrorWithCStr("byte string is too large");
+    return nullptr;
+  }
+
+  return PyBytes_FromStringAndSize(str, static_cast<word>(size));
 }
 
 PY_EXPORT PyObject* PyBytes_Repr(PyObject* /* j */, int /* s */) {
