9ea0b2729e33e2033b42357deac6b95e3edebbb7
Carl Shapiro <cshapiro@fb.com>
2018-11-28 10:34:15 -0800

    Implement PyOS_snprintf and PyOS_vsnprintf
    These wrap the system snprintf and vsnprintf with added behavior.
    First, pointer arguments are asserted to be non-NULL and the size
    is checked to be non-zero.  Second, when the size is greater than
    INT_MAX-1 the system exits early and terminate the buffer with a
    '\0'.  The latter case cannot be tested.
    
    Based on Facebook D13148612

---
+++ ext/Python/mysnprintf-test.cpp
@@ -0,0 +1,26 @@
+#include "gtest/gtest.h"
+
+#include <cstring>
+
+#include "Python.h"
+#include "capi-testing.h"
+
+namespace python {
+
+TEST(MysnprintfTest, Snprintf) {
+  char str[5];
+
+  std::memset(str, 0xFF, sizeof(str));
+  EXPECT_EQ(PyOS_snprintf(str, sizeof(str), "%d", 123), 3);
+  EXPECT_STREQ(str, "123");
+
+  std::memset(str, 0xFF, sizeof(str));
+  EXPECT_EQ(PyOS_snprintf(str, sizeof(str), "%d", 1234), 4);
+  EXPECT_STREQ(str, "1234");
+
+  std::memset(str, 0xFF, sizeof(str));
+  EXPECT_EQ(PyOS_snprintf(str, sizeof(str), "%d", 12345678), 8);
+  EXPECT_STREQ(str, "1234");
+}
+
+}  // namespace python
--- ext/Python/mysnprintf.cpp
+++ ext/Python/mysnprintf.cpp
@@ -1,15 +1,31 @@
-#include "runtime.h"
+#include <climits>
+#include <cstdarg>
+#include <cstdio>
+
+#include "cpython-func.h"
+#include "globals.h"
+#include "utils.h"
 
 namespace python {
 
-PY_EXPORT int PyOS_snprintf(char* /* r */, size_t /* e */, const char* /* t */,
-                            ...) {
-  UNIMPLEMENTED("PyOS_snprintf");
+PY_EXPORT int PyOS_snprintf(char* str, size_t size, const char* format, ...) {
+  va_list va;
+  va_start(va, format);
+  int result = PyOS_vsnprintf(str, size, format, va);
+  va_end(va);
+  return result;
 }
 
-PY_EXPORT int PyOS_vsnprintf(char* /* r */, size_t /* e */, const char* /* t */,
-                             va_list /* a */) {
-  UNIMPLEMENTED("PyOS_vsnprintf");
+PY_EXPORT int PyOS_vsnprintf(char* str, size_t size, const char* format,
+                             va_list va) {
+  DCHECK(str != nullptr, "string to write to cannot be null");
+  DCHECK(size > 0, "number of characters to write must be positive");
+  DCHECK(format != nullptr, "format string cannot be null");
+  if (size > (INT_MAX - 1)) {
+    str[size - 1] = '\0';
+    return -666;
+  }
+  return std::vsnprintf(str, size, format, va);
 }
 
 }  // namespace python
