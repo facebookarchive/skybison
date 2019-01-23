d6c2392f9bccae121784218ba94e07ad92da1b2c
Antony Courtney <antonycourtney@fb.com>
2019-01-22 16:09:41 -0800

    RFC: Call Runtime::compile from main when run on Python sources
    Summary: Call Runtime::compile to shell out to CPython to byte-code compile Python sources before interpreting bytecodes. Reduces friction for quick investigations and enables simple diff tests against CPython.
    
    Based on Facebook D13743549

--- main.cpp
+++ main.cpp
@@ -10,9 +10,23 @@ int main(int argc, const char** argv) {
   }
   python::Runtime runtime;
   runtime.setArgv(argc, argv);
-  std::unique_ptr<char[]> buffer(python::OS::readFile(argv[1]));
+  const char* file_name = argv[1];
+  std::unique_ptr<char[]> buffer(python::OS::readFile(file_name));
   if (buffer.get() == nullptr) {
     std::exit(EXIT_FAILURE);
+  }
+  bool is_source(true);
+
+  // Interpret as bytecode iff file name suffix is '.pyc':
+  const char* delim = strrchr(file_name, '.');
+  if (delim) {
+    const char* ext = delim + 1;
+    if (strcmp(ext, "pyc") == 0) {
+      is_source = false;
+    }
+  }
+  if (is_source) {
+    buffer = python::Runtime::compile(buffer.get());
   }
   runtime.run(buffer.get());
   return EXIT_SUCCESS;
