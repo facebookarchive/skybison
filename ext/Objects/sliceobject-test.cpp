b6082c2ec92049f82886dae221e0848d48c14059
William Meehan <wmeehan@fb.com>
2019-02-08 07:21:12 -0800

    Implement remaining PySlice API functions
    Implement PySlice_AdjustIndices, PySlice_GetIndicesEx, and PySlice_Unpack.
    Leave PySlice_GetIndices unimplemented because it has no callers in the CPython builtin modules and the documentation has said "You probably do not want to use this function" since Python 2.
    
    Based on Facebook D13975234

--- ext/Objects/sliceobject-test.cpp
+++ ext/Objects/sliceobject-test.cpp
@@ -15,4 +15,187 @@ TEST_F(SliceExtensionApiTest, NewReturnsSlice) {
   EXPECT_TRUE(PySlice_Check(slice));
 }
 
+TEST_F(SliceExtensionApiTest, AdjustIndicesReturnsSliceLength) {
+  Py_ssize_t length = 10;
+  Py_ssize_t start = 2;
+  Py_ssize_t stop = -1;
+  Py_ssize_t step = 3;
+  Py_ssize_t slice_length = PySlice_AdjustIndices(length, &start, &stop, step);
+  EXPECT_EQ(slice_length, 3);
+  EXPECT_EQ(start, 2);
+  EXPECT_EQ(stop, 9);
+}
+
+TEST_F(SliceExtensionApiTest, AdjustIndicesWithNegativeShifts) {
+  Py_ssize_t length = 5;
+  Py_ssize_t start = -4;
+  Py_ssize_t stop = -1;
+  Py_ssize_t step = 1;
+  Py_ssize_t slice_length = PySlice_AdjustIndices(length, &start, &stop, step);
+  EXPECT_EQ(slice_length, 3);
+  EXPECT_EQ(start, 1);
+  EXPECT_EQ(stop, 4);
+}
+
+TEST_F(SliceExtensionApiTest,
+       AdjustIndicesWithLargeNegativesAndPositiveStepSetsZero) {
+  Py_ssize_t length = 5;
+  Py_ssize_t start = -40;
+  Py_ssize_t stop = -10;
+  Py_ssize_t step = 2;
+  Py_ssize_t slice_length = PySlice_AdjustIndices(length, &start, &stop, step);
+  EXPECT_EQ(slice_length, 0);
+  EXPECT_EQ(start, 0);
+  EXPECT_EQ(stop, 0);
+}
+
+TEST_F(SliceExtensionApiTest,
+       AdjustIndicesWithLargeNegativesAndNegativeStepSetsNegativeOne) {
+  Py_ssize_t length = 5;
+  Py_ssize_t start = -40;
+  Py_ssize_t stop = -100;
+  Py_ssize_t step = -2;
+  Py_ssize_t slice_length = PySlice_AdjustIndices(length, &start, &stop, step);
+  EXPECT_EQ(slice_length, 0);
+  EXPECT_EQ(start, -1);
+  EXPECT_EQ(stop, -1);
+}
+
+TEST_F(SliceExtensionApiTest, AdjustIndicesWithLargeIndicesSetsLength) {
+  Py_ssize_t length = 5;
+  Py_ssize_t start = 8;
+  Py_ssize_t stop = 10;
+  Py_ssize_t step = 1;
+  Py_ssize_t slice_length = PySlice_AdjustIndices(length, &start, &stop, step);
+  EXPECT_EQ(slice_length, 0);
+  EXPECT_EQ(start, 5);
+  EXPECT_EQ(stop, 5);
+}
+
+TEST_F(SliceExtensionApiTest,
+       AdjustIndicesWithLargeIndicesAndNegativeStepSetsOffsetLength) {
+  Py_ssize_t length = 5;
+  Py_ssize_t start = 8;
+  Py_ssize_t stop = 10;
+  Py_ssize_t step = -1;
+  Py_ssize_t slice_length = PySlice_AdjustIndices(length, &start, &stop, step);
+  EXPECT_EQ(slice_length, 0);
+  EXPECT_EQ(start, 4);
+  EXPECT_EQ(stop, 4);
+}
+
+TEST_F(SliceExtensionApiTest, GetIndicesExWithUnpackErrorRaisesValueError) {
+  PyObjectPtr zero(PyLong_FromLong(0));
+  PyObjectPtr slice(PySlice_New(nullptr, nullptr, zero));
+  Py_ssize_t length = 0;
+  Py_ssize_t start, stop, step, slicelength;
+  int result =
+      PySlice_GetIndicesEx(slice, length, &start, &stop, &step, &slicelength);
+  ASSERT_EQ(result, -1);
+  ASSERT_NE(PyErr_Occurred(), nullptr);
+  EXPECT_TRUE(PyErr_ExceptionMatches(PyExc_ValueError));
+}
+
+TEST_F(SliceExtensionApiTest, GetIndicesExWithUnpackSuccessSetsValues) {
+  PyObjectPtr two(PyLong_FromLong(2));
+  PyObjectPtr slice(PySlice_New(nullptr, nullptr, two));
+  Py_ssize_t length = 10;
+  Py_ssize_t start, stop, step, slicelength;
+  int result =
+      PySlice_GetIndicesEx(slice, length, &start, &stop, &step, &slicelength);
+  ASSERT_EQ(result, 0);
+  ASSERT_EQ(PyErr_Occurred(), nullptr);
+  EXPECT_EQ(start, 0);
+  EXPECT_EQ(stop, 10);
+  EXPECT_EQ(step, 2);
+  EXPECT_EQ(slicelength, 5);
+}
+
+TEST_F(SliceExtensionApiTest, UnpackWithNonSliceRaisesSystemErrorPyro) {
+  PyObjectPtr num(PyLong_FromLong(0));
+  Py_ssize_t start, stop, step;
+  ASSERT_EQ(PySlice_Unpack(num, &start, &stop, &step), -1);
+  ASSERT_NE(PyErr_Occurred(), nullptr);
+  EXPECT_TRUE(PyErr_ExceptionMatches(PyExc_SystemError));
+}
+
+TEST_F(SliceExtensionApiTest, UnpackWithNonIndexStartRaisesTypeError) {
+  PyObjectPtr list(PyList_New(0));
+  PyObjectPtr slice(PySlice_New(list, nullptr, nullptr));
+  Py_ssize_t start, stop, step;
+  ASSERT_EQ(PySlice_Unpack(slice, &start, &stop, &step), -1);
+  ASSERT_NE(PyErr_Occurred(), nullptr);
+  EXPECT_TRUE(PyErr_ExceptionMatches(PyExc_TypeError));
+}
+
+TEST_F(SliceExtensionApiTest, UnpackWithNonIndexStopRaisesTypeError) {
+  PyObjectPtr list(PyList_New(0));
+  PyObjectPtr slice(PySlice_New(nullptr, list, nullptr));
+  Py_ssize_t start, stop, step;
+  ASSERT_EQ(PySlice_Unpack(slice, &start, &stop, &step), -1);
+  ASSERT_NE(PyErr_Occurred(), nullptr);
+  EXPECT_TRUE(PyErr_ExceptionMatches(PyExc_TypeError));
+}
+
+TEST_F(SliceExtensionApiTest, UnpackWithNonIndexStepRaisesTypeError) {
+  PyObjectPtr list(PyList_New(0));
+  PyObjectPtr slice(PySlice_New(nullptr, nullptr, list));
+  Py_ssize_t start, stop, step;
+  ASSERT_EQ(PySlice_Unpack(slice, &start, &stop, &step), -1);
+  ASSERT_NE(PyErr_Occurred(), nullptr);
+  EXPECT_TRUE(PyErr_ExceptionMatches(PyExc_TypeError));
+}
+
+TEST_F(SliceExtensionApiTest, UnpackWithZeroStepRaisesTypeError) {
+  PyObjectPtr zero(PyLong_FromLong(0));
+  PyObjectPtr slice(PySlice_New(nullptr, nullptr, zero));
+  Py_ssize_t start, stop, step;
+  ASSERT_EQ(PySlice_Unpack(slice, &start, &stop, &step), -1);
+  ASSERT_NE(PyErr_Occurred(), nullptr);
+  EXPECT_TRUE(PyErr_ExceptionMatches(PyExc_ValueError));
+}
+
+TEST_F(SliceExtensionApiTest, UnpackWithNoneSetsDefaults) {
+  PyObjectPtr slice(PySlice_New(nullptr, nullptr, nullptr));
+  Py_ssize_t start, stop, step;
+  ASSERT_EQ(PySlice_Unpack(slice, &start, &stop, &step), 0);
+  ASSERT_EQ(PyErr_Occurred(), nullptr);
+  EXPECT_EQ(start, 0);
+  EXPECT_GT(stop, 1 << 30);  // Arbitrary large value less than
+                             // PY_SSIZE_T_MAX and SmallInt::kMaxValue
+  EXPECT_EQ(step, 1);
+}
+
+TEST_F(SliceExtensionApiTest, UnpackWithNonIntCallsDunderIndex) {
+  PyRun_SimpleString(R"(
+class Foo:
+  def __init__(self): self.bar = 0
+  def __index__(self):
+    self.bar += 1
+    return self.bar
+foo = Foo()
+)");
+  PyObjectPtr foo(moduleGet("__main__", "foo"));
+  PyObjectPtr slice(PySlice_New(foo, foo, foo));
+  Py_ssize_t start, stop, step;
+  ASSERT_EQ(PySlice_Unpack(slice, &start, &stop, &step), 0);
+  ASSERT_EQ(PyErr_Occurred(), nullptr);
+  EXPECT_EQ(start, 2);
+  EXPECT_EQ(stop, 3);
+  EXPECT_EQ(step, 1);
+}
+
+TEST_F(SliceExtensionApiTest, UnpackWithIndicesSetsValues) {
+  PyObjectPtr idx1(PyLong_FromLong(1024));
+  PyObjectPtr idx2(PyLong_FromLong(-42));
+  PyObjectPtr idx3(PyLong_FromLong(10));
+  PyObjectPtr slice(PySlice_New(idx1, idx2, idx3));
+  Py_ssize_t start, stop, step;
+  ASSERT_EQ(PySlice_Unpack(slice, &start, &stop, &step), 0);
+  ASSERT_EQ(PyErr_Occurred(), nullptr);
+  EXPECT_EQ(start, 1024);
+  EXPECT_GE(stop, -42);
+  EXPECT_EQ(step, 10);
+}
+
 }  // namespace python
--- ext/Objects/sliceobject.cpp
+++ ext/Objects/sliceobject.cpp
@@ -1,15 +1,11 @@
+#include "cpython-func.h"
 #include "runtime.h"
+#include "slice-builtins.h"
 
 namespace python {
 
 PY_EXPORT int PySlice_Check_Func(PyObject* pyobj) {
   return ApiHandle::fromPyObject(pyobj)->asObject()->isSlice();
-}
-
-PY_EXPORT int PySlice_GetIndices(PyObject* /* r */, Py_ssize_t /* h */,
-                                 Py_ssize_t* /* t */, Py_ssize_t* /* p */,
-                                 Py_ssize_t* /* p */) {
-  UNIMPLEMENTED("PySlice_GetIndices");
 }
 
 PY_EXPORT PyObject* PySlice_New(PyObject* start, PyObject* stop,
@@ -29,22 +25,55 @@ PY_EXPORT PyObject* PySlice_New(PyObject* start, PyObject* stop,
   return ApiHandle::newReference(thread, *slice);
 }
 
-PY_EXPORT Py_ssize_t PySlice_AdjustIndices(Py_ssize_t /* h */,
-                                           Py_ssize_t* /* t */,
-                                           Py_ssize_t* /* p */,
-                                           Py_ssize_t /* p */) {
-  UNIMPLEMENTED("PySlice_AdjustIndices");
+PY_EXPORT Py_ssize_t PySlice_AdjustIndices(Py_ssize_t length,
+                                           Py_ssize_t* start_ptr,
+                                           Py_ssize_t* stop_ptr,
+                                           Py_ssize_t step) {
+  DCHECK(step != 0, "step cannot be 0");
+  DCHECK(step >= -SmallInt::kMaxValue, "step must allow for safe reversal");
+  DCHECK(length >= 0, "length cannot be negative");
+  word start = static_cast<word>(*start_ptr);
+  word stop = static_cast<word>(*stop_ptr);
+  word slice_length = RawSlice::adjustIndices(static_cast<word>(length), &start,
+                                              &stop, static_cast<word>(step));
+  *start_ptr = static_cast<Py_ssize_t>(start);
+  *stop_ptr = static_cast<Py_ssize_t>(stop);
+  return static_cast<Py_ssize_t>(slice_length);
 }
 
-PY_EXPORT int PySlice_GetIndicesEx(PyObject* /* r */, Py_ssize_t /* h */,
-                                   Py_ssize_t* /* t */, Py_ssize_t* /* p */,
-                                   Py_ssize_t* /* p */, Py_ssize_t* /* h */) {
-  UNIMPLEMENTED("PySlice_GetIndicesEx");
+PY_EXPORT int PySlice_GetIndices(PyObject*, Py_ssize_t, Py_ssize_t*,
+                                 Py_ssize_t*, Py_ssize_t*) {
+  UNIMPLEMENTED("PySlice_GetIndices is not supported");
 }
 
-PY_EXPORT int PySlice_Unpack(PyObject* /* r */, Py_ssize_t* /* t */,
-                             Py_ssize_t* /* p */, Py_ssize_t* /* p */) {
-  UNIMPLEMENTED("PySlice_Unpack");
+PY_EXPORT int PySlice_GetIndicesEx(PyObject* slice, Py_ssize_t length,
+                                   Py_ssize_t* start, Py_ssize_t* stop,
+                                   Py_ssize_t* step, Py_ssize_t* slicelength) {
+  if (PySlice_Unpack(slice, start, stop, step) < 0) {
+    return -1;
+  }
+  *slicelength = PySlice_AdjustIndices(length, start, stop, *step);
+  return 0;
+}
+
+PY_EXPORT int PySlice_Unpack(PyObject* pyobj, Py_ssize_t* start_ptr,
+                             Py_ssize_t* stop_ptr, Py_ssize_t* step_ptr) {
+  DCHECK(SmallInt::kMinValue + 1 <= -SmallInt::kMaxValue);
+  Thread* thread = Thread::currentThread();
+  HandleScope scope(thread);
+  Object obj(&scope, ApiHandle::fromPyObject(pyobj)->asObject());
+  if (!obj.isSlice()) {
+    thread->raiseBadInternalCall();
+    return -1;
+  }
+  Slice slice(&scope, *obj);
+  word start, stop, step;
+  Object err(&scope, sliceUnpack(thread, slice, &start, &stop, &step));
+  if (err.isError()) return -1;
+  *start_ptr = static_cast<Py_ssize_t>(start);
+  *stop_ptr = static_cast<Py_ssize_t>(stop);
+  *step_ptr = static_cast<Py_ssize_t>(step);
+  return 0;
 }
 
 }  // namespace python
