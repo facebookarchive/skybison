a0575a83c91a20e5f36c8b878ee5274a70513938
Carl Shapiro <cshapiro@fb.com>
2019-02-14 16:56:00 -0800

    Add the accumulator type to the build
    Summary: This interface is used by StringIO and in other places.
    
    Based on Facebook D14092846

--- CPythonBuild.cmake
+++ CPythonBuild.cmake
@@ -76,6 +76,7 @@ set(
 	CPYTHON_SOURCES
 	${CPYTHON_SOURCES}
   ${CPYTHON_SOURCES_WITH_CLINIC_ANNOTATIONS}
+  "${CPYTHON_DIR}/Objects/accu.c"
   "${CPYTHON_DIR}/Python/fileutils.c"
   "${CPYTHON_DIR}/Python/pyctype.c"
   "${CPYTHON_DIR}/Python/pytime.c"
