456492e4781c1756dcc359a19f4ef358c5d2e558
Carl Shapiro <cshapiro@fb.com>
2019-02-19 10:01:53 -0800

    Add string to long conversion functions
    These are Python's string to long and string to unsigned long
    routines, similar to strtol and strtoul in the standard library.
    
    Based on Facebook D14097994

--- ext/Python/mystrtoul.cpp
+++
@@ -1,9 +0,0 @@
-#include "runtime.h"
-
-namespace python {
-
-PY_EXPORT long PyOS_strtol(const char* /* r */, char** /* ptr */, int /* e */) {
-  UNIMPLEMENTED("PyOS_strtol");
-}
-
-}  // namespace python
