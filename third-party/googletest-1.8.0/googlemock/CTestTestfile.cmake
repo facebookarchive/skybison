c4fab458ef18d9116fa96acc7391aff370e06933
Matthias Braun <matthiasb@fb.com>
2019-02-26 13:48:59 -0800

    Update to googletest-1.8.1
    Updating to get `-Wmissing-declarations` warning fixes.
    
    While on it also remove a couple cmake generated `CMakeFiles` that
    should not have been checked into the repository.
    
    @allow_many_files
    
    Based on Facebook D14212086

--- third-party/googletest-1.8.0/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mpage/src/shapyro/python/third-party/googletest-1.8.0/googlemock
-# Build directory: /Users/mpage/src/shapyro/python/third-party/googletest-1.8.0/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- third-party/googletest-1.8.0/googlemock/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/mpage/src/shapyro/python/third-party/googletest-1.8.0/googletest
-# Build directory: /Users/mpage/src/shapyro/python/third-party/googletest-1.8.0/googlemock/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
