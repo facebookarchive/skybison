350d64467ab4cfe6d9b73f682185efe70e0e286a
Carl Shapiro <cshapiro@fb.com>
2019-03-25 19:52:00 -0700

    Remove a dead method declaration
    This seems to be leftover from when the module template refactoring
    that removed the postInitialize method.
    
    Based on Facebook D14625434

--- runtime/time-module.h
+++ runtime/time-module.h
@@ -9,8 +9,6 @@ namespace python {
 
 class TimeModule : public ModuleBase<TimeModule, SymbolId::kTime> {
  public:
-  static void postInitialize(Thread* thread, Runtime* runtime,
-                             const Module& module);
   static RawObject time(Thread* thread, Frame* frame, word nargs);
 
   static const BuiltinMethod kBuiltinMethods[];
