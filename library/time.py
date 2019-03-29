36f36b0a8b359c9ec191132071fb27e1ee420ce5
Matthias Braun <matthiasb@fb.com>
2019-03-28 19:52:00 -0700

    Add stubs in time module
    Based on Facebook D14688748

--- library/time.py
+++ library/time.py
@@ -26,9 +26,84 @@ if it is -1, mktime() should guess based on the date and time."""
 # This is the patch decorator, injected by our boot process. flake8 has no
 # knowledge about its definition and will complain without this gross circular
 # helper here.
+_Unbound = _Unbound  # noqa: F821
 _patch = _patch  # noqa: F821
+_unimplemented = _unimplemented  # noqa: F821
+
+
+def asctime(tuple=_Unbound):
+    _unimplemented()
+
+
+def clock():
+    _unimplemented()
+
+
+def clock_getres(clk_id):
+    _unimplemented()
+
+
+def clock_gettime(clk_id):
+    _unimplemented()
+
+
+def clock_settime(clk_id, time):
+    _unimplemented()
+
+
+def ctime(seconds):
+    _unimplemented()
+
+
+def get_clock_info(name):
+    _unimplemented()
+
+
+def gmtime(seconds=_Unbound):
+    _unimplemented()
+
+
+def localtime(seconds=_Unbound):
+    _unimplemented()
+
+
+def mktime(tuple):
+    _unimplemented()
+
+
+def monotonic():
+    _unimplemented()
+
+
+def perf_counter():
+    _unimplemented()
+
+
+def process_time():
+    _unimplemented()
+
+
+def sleep(seconds):
+    _unimplemented()
+
+
+def strftime(format, tuple=_Unbound):
+    _unimplemented()
+
+
+def strptime(string, format):
+    _unimplemented()
+
+
+class struct_time:
+    def __init__(self, *args, **kwargs):
+        _unimplemented()
 
 
 @_patch
 def time():
     pass
+
+
+def tzset():
+    _unimplemented()
