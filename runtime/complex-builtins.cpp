71a543e0b124d7a8557cd64f0e6e7df42f0538ff
Max Bernstein <emacs@fb.com>
2019-03-28 19:52:00 -0700

    Unify complex builtins type error reporting
    Based on Facebook D14676038

--- runtime/complex-builtins.cpp
+++ runtime/complex-builtins.cpp
@@ -18,8 +18,7 @@ RawObject complexGetImag(Thread* thread, Frame* frame, word nargs) {
   Object self_obj(&scope, args.get(0));
   Runtime* runtime = thread->runtime();
   if (!runtime->isInstanceOfComplex(*self_obj)) {
-    return thread->raiseTypeErrorWithCStr(
-        "_complex_imag requires a complex instance");
+    return thread->raiseRequiresType(self_obj, SymbolId::kComplex);
   }
   Complex self(&scope, *self_obj);
   return runtime->newFloat(self.imag());
@@ -31,8 +30,7 @@ RawObject complexGetReal(Thread* thread, Frame* frame, word nargs) {
   Object self_obj(&scope, args.get(0));
   Runtime* runtime = thread->runtime();
   if (!runtime->isInstanceOfComplex(*self_obj)) {
-    return thread->raiseTypeErrorWithCStr(
-        "_complex_real requires a complex instance");
+    return thread->raiseRequiresType(self_obj, SymbolId::kComplex);
   }
   Complex self(&scope, *self_obj);
   return runtime->newFloat(self.real());
