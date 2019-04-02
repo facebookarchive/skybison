b56429ba7e5df3f213dbf27c8e847608d4009d47
Carl Shapiro <cshapiro@fb.com>
2019-04-01 19:52:00 -0700

    Fix some bad indentation in a CMake file
    Summary: Tabs somehow crept into my previous commit
    
    Based on Facebook D14727853

--- CPythonBuild.cmake
+++ CPythonBuild.cmake
@@ -73,12 +73,12 @@ set(
 
 # Add some Python files all the core Modules sources
 set(
-	CPYTHON_SOURCES
-	${CPYTHON_SOURCES}
+  CPYTHON_SOURCES
+  ${CPYTHON_SOURCES}
   ${CPYTHON_SOURCES_WITH_CLINIC_ANNOTATIONS}
   "${CPYTHON_DIR}/Objects/accu.c"
-	"${CPYTHON_DIR}/Python/dtoa.c"
-	"${CPYTHON_DIR}/Python/fileutils.c"
+  "${CPYTHON_DIR}/Python/dtoa.c"
+  "${CPYTHON_DIR}/Python/fileutils.c"
   "${CPYTHON_DIR}/Python/mystrtoul.c"
   "${CPYTHON_DIR}/Python/pyarena.c"
   "${CPYTHON_DIR}/Python/pyctype.c"
