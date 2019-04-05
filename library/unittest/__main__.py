046aa3d490914b52b3d055ca7c64e9b6f457ebc2
Eddie Elizondo <eelizondo@fb.com>
2019-04-04 19:52:00 -0700

    Copy unittest into library
    Summary: Copy unittest module from cpython library so we can subsequently modify it.
    
    Based on Facebook D14803131

---
+++ library/unittest/__main__.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# WARNING: This is a temporary copy of code from the cpython library to
+# facilitate bringup. Please file a task for anything you change!
+# flake8: noqa
+# fmt: off
+"""Main entry point"""
+
+import sys
+
+from .main import TestProgram, main
+
+
+if sys.argv[0].endswith("__main__.py"):
+    import os.path
+    # We change sys.argv[0] to make help message more useful
+    # use executable without path, unquoted
+    # (it's just a hint anyway)
+    # (if you have spaces in your executable you get what you deserve!)
+    executable = os.path.basename(sys.executable)
+    sys.argv[0] = executable + " -m unittest"
+    del os
+
+__unittest = True
+
+
+main(module=None)
