6cfb887fd803bdc742f7c7c512324a277a328c99
Eddie Elizondo <eelizondo@fb.com>
2019-04-07 19:52:00 -0700

    Copy re.py and dependencies and stub _sre.py
    Summary: Copy re module and its dependencies from cpython library so we can subsequently modify it. This also adds a stub for _sre to mark as unimplemented for now.
    
    Based on Facebook D14803333

---
+++ library/_sre.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+_unimplemented = _unimplemented  # noqa: F821
+
+
+CODESIZE = 4
+
+
+MAGIC = 20140917
+
+
+MAXGROUPS = 2147483647
+
+
+MAXREPEAT = 4294967295
+
+
+def compile(*args, **kwargs):
+    _unimplemented()
+
+
+def getcodesize(*args, **kwargs):
+    _unimplemented()
+
+
+def getlower(*args, **kwargs):
+    _unimplemented()
