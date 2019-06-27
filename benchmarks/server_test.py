f1de58d13b331ca5e8ec7726ada2ac5fabe2747b
Eddie Elizondo <eelizondo@fb.com>
2019-06-26 19:52:00 -0700

    Add blob storage to send binary from client to server
    This enables the client to send a binary to the server by using a blob storage service.
    
    The client will get the path to the binary it wants to benchmark. This will be sent to Everstore which will return a handle. This handle will then be passed to the server which will use it to download and make an executable binary out of that blob.
    
    This uses the newly created `EVERSTORE_PYRO_BINARIES` FbType which have a retention rate of 1 day: https://our.intern.facebook.com/intern/everstore/customer/2136/.
    
    Based on Facebook D15995323

--- benchmarks/server_test.py
+++ benchmarks/server_test.py
@@ -4,12 +4,30 @@ from libfb.py.testutil import BaseFacebookTestCase
 from pyro.benchmarks.server_handler import PyroBenchmarksServer
 
 
+class MockBlobStorage:
+    def __init__(self, blob_handle):
+        self.blob_handle = blob_handle
+
+    async def store_blob(self, file_path):
+        return self.blob_handle
+
+    async def download_blob(self, blob_handle, _file_path):
+        return self.blob_handle == blob_handle
+
+
 class PyroBenchmarksTestCase(BaseFacebookTestCase):
     def setUp(self):
-        self.controller = PyroBenchmarksServer()
+        self.mock_blob_handle = "foo_bar_baz"
+        storage_client = MockBlobStorage(self.mock_blob_handle)
+        self.controller = PyroBenchmarksServer(storage_client)
         BaseFacebookTestCase.setUp(self)
 
     @async_test
-    async def test_hello(self):
-        result = await self.controller.hello()
-        self.assertEqual(result, "world")
+    async def test_run_completes(self):
+        result = await self.controller.run(self.mock_blob_handle)
+        self.assertEqual(result, "success")
+
+    @async_test
+    async def test_run_fails(self):
+        result = await self.controller.run("bad blob")
+        self.assertEqual(result, "failure")
