e4d2500991a96d5b1abf67faf57e01edca0d1b52
Matthias Braun <matthiasb@fb.com>
2019-07-15 19:52:00 -0700

    Add platform007.cmake pre-load script
    Add cmake script that to be used with the `-C` flag of `cmake` that configures a build suitable for deployment on facebook platform007 machines.
    
    This is not meant to be the final word on decisions like clang vs gcc, what optimization flags to use,
    using libraries from `/usr/local/fbcode/platform007/lib/` vs `/lib64` etc. We can tweak those settings
    in follow-ups.
    
    Based on Facebook D16182932

---
+++ util/platform007.cmake
@@ -0,0 +1,64 @@
+# Toolchain settings for facebook platform007
+# This should be used with the `-C` option. Example:
+#     $ cmake -C util/platform007.cmake ..
+#
+# See also:
+#   fbsource/tools/buckconfigs/fbcode/platforms/*/cxx.bcfg
+
+# This is the same as `string(JOIN)` which is only available in cmake >= 3.12
+function(string_join GLUE OUT_VAR)
+  set(result "")
+  set(glue "")
+  foreach(e ${ARGN})
+    set(result "${result}${glue}${e}")
+    set(glue "${GLUE}")
+  endforeach()
+  set(${OUT_VAR} "${result}" PARENT_SCOPE)
+endfunction()
+
+# Determine fbcode checkout directory.
+execute_process(COMMAND hg root WORKING_DIRECTORY "${CMAKE_CURRENT_LIST_DIR}"
+  OUTPUT_VARIABLE FBSOURCE_DIR OUTPUT_STRIP_TRAILING_WHITESPACE
+  RESULT_VARIABLE RES)
+if (NOT RES EQUAL 0)
+  message(FATAL_ERROR "Could not determine fbsource checkout directory")
+endif()
+
+set(FBCODE_DIR "${FBSOURCE_DIR}/fbcode")
+set(LLVM_BIN "${FBCODE_DIR}/third-party-buck/platform007/tools/llvm-fb/bin")
+
+# Default to release build
+set(CMAKE_BUILD_TYPE "Release" CACHE STRING "")
+
+set(CMAKE_C_COMPILER "/usr/local/fbcode/platform007/bin/clang.par" CACHE STRING "")
+set(CMAKE_CXX_COMPILER "/usr/local/fbcode/platform007/bin/clang++.par" CACHE STRING "")
+set(CMAKE_LINKER "${LLVM_BIN}/lld" CACHE STRING "")
+set(CMAKE_AR "${LLVM_BIN}/llvm-ar" CACHE STRING "")
+set(CMAKE_NM "${LLVM_BIN}/llvm-nm" CACHE STRING "")
+set(CMAKE_RANLIB "/bin/true" CACHE STRING "")
+set(CMAKE_OBJCOPY "${LLVM_BIN}/llvm-objcopy" CACHE STRING "")
+set(CMAKE_OBJDUMP "${LLVM_BIN}/llvm-objdump" CACHE STRING "")
+
+string_join(" " PLATFORM_COMPILER_FLAGS
+  --platform=platform007
+)
+string_join(" " PLATFORM_LINKER_FLAGS
+  --platform=platform007
+  -Wl,--dynamic-linker,/usr/local/fbcode/platform007/lib/ld.so
+  -Wl,-rpath,/usr/local/fbcode/platform007/lib
+  -fuse-ld=lld       # Use lld because gnu ld does not support clang LTO
+  -B${LLVM_BIN}      # path to linker
+  -Wl,--emit-relocs  # needed for bolt
+)
+set(PLATFORM_COMPILER_FLAGS "${PLATFORM_COMPILER_FLAGS}" CACHE STRING "")
+set(PLATFORM_LINKER_FLAGS "${PLATFORM_LINKER_FLAGS}" CACHE STRING "")
+
+string_join(" " FLAGS_RELEASE
+  -O3
+  -DNDEBUG
+  -march=corei7
+  -mtune=core-avx2
+  -mpclmul
+)
+set(CMAKE_C_FLAGS_RELEASE "${FLAGS_RELEASE}" CACHE STRING "")
+set(CMAKE_CXX_FLAGS_RELEASE "${FLAGS_RELEASE}" CACHE STRING "")
