9196eb50d2b62e89d960efb1de906502026d4b6b
Eddie Elizondo <eelizondo@fb.com>
2019-07-30 19:52:00 -0700

    Add performance delta table in pyro_perf_sigal
    This adds an extra table in pyro perf signal which shows the performance delta in percentage of each of the fields.
    
    This compares each interpreter against the last one in the list (in this case, cpython vs python_new, and python_base vs python_new)
    
    Based on Facebook D16508207

--- benchmarks/_display_results.py
+++ benchmarks/_display_results.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
+import logging
+
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger(__name__)
 
 
 MD_ROW_SEP = "-"
@@ -11,6 +16,36 @@ def _format_value(value):
     if isinstance(value, int):
         return f"{round(value, 2):,d}"
     return value
+
+
+def build_perf_delta_table(benchmark_results, num_interpreters):
+    # Deltas are done with every interpreter against the last intereter
+    # Formula: ((new / old) - 1) * 100
+    results = []
+    benchmark_results.sort(key=lambda x: x["benchmark"], reverse=True)
+    last_interpreter = benchmark_results[num_interpreters - 1 :: num_interpreters]
+    for idx in range(0, num_interpreters - 1):
+        current_interpreter = benchmark_results[idx::num_interpreters]
+        for old, new in zip(current_interpreter, last_interpreter):
+            result = {
+                "benchmark": old["benchmark"],
+                "compare": f"{old['interpreter']} vs {new['interpreter']}",
+            }
+            for k, v in old.items():
+                # Ignore any results that are not present in both interpreters
+                if k not in new.keys():
+                    log.error(f"Metric: {k} is not present in {new['interpreter']}")
+                    continue
+                # Only compare numerical values
+                if not isinstance(v, int) and not isinstance(v, float):
+                    continue
+                percent = ((new[k] / old[k]) - 1) * 100
+                if percent >= 0:
+                    result[k] = f"**{round(percent, 2)}%**"
+                else:
+                    result[k] = f"{round(percent, 2)}%"
+            results.append(result)
+    return build_table(results)
 
 
 def build_table(results):
