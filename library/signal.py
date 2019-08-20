1a5caecb420cfd6c073b23833e4ccf3a0d525b30
Matthias Braun <matthiasb@fb.com>
2019-08-19 19:52:00 -0700

    Add stubbed out signal module
    Summary: Add a stubbed out signal module to unblock code that imports the signal module but does not always call a function in it.
    
    Based on Facebook D16918296

---
+++ library/signal.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+"""Temporary stub functions until we have a working _signal module."""
+
+
+# These values are injected by our boot process. flake8 has no knowledge about
+# their definitions and will complain without this circular helper here.
+_unimplemented = _unimplemented  # noqa: F821
+
+
+def alarm(seconds):
+    _unimplemented()
+
+
+def default_int_handler(*args, **kwargs):
+    _unimplemented()
+
+
+def getitimer(which):
+    _unimplemented()
+
+
+def pause():
+    _unimplemented()
+
+
+def pthread_kill(thread_id, signalnum):
+    _unimplemented()
+
+
+def set_wakeup_fd(fd):
+    _unimplemented()
+
+
+def setitimer(which, seconds, interval=0.0):
+    _unimplemented()
+
+
+def siginterrupt(signalnum, flag):
+    _unimplemented()
+
+
+def signal(signalnum, handler):
+    _unimplemented()
+
+
+def sigpending():
+    _unimplemented()
+
+
+def sigwait(sigset):
+    _unimplemented()
