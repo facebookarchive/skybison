e1edf8eef2b262fd47cdfe2b9339ee3f6d06e374
Carl Shapiro <cshapiro@fb.com>
2019-08-20 19:52:00 -0700

    Implement a do-nothing gc module
    Summary: These are just stubs which should be enough for now.
    
    Based on Facebook D16930090

---
+++ library/gc.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+_unimplemented = _unimplemented  # noqa: F821
+
+
+def disable():
+    pass
+
+
+def enable():
+    _unimplemented()
+
+
+def isenabled():
+    return False
