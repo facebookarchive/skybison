89d94787b5d47755fb21abcc0159553d0c32b294
Max Bernstein <emacs@fb.com>
2019-09-09 19:52:00 -0700

    Back out "Fork importlib.machinery into library"
    This should be unnecessary. I happened to have a dirty `library` folder
    which caused this problem. It's not a problem for anyone else.
    
    Original commit changeset: 2e4f5917da39
    
    Based on Facebook D17296827

--- library/importlib_test.py
+++ library/importlib_test.py
@@ -8,6 +8,11 @@ class ImportlibTests(unittest.TestCase):
         warnings_module = importlib.import_module("warnings")
         self.assertEqual(warnings_module.__name__, "warnings")
 
+    def test_import_machinery(self):
+        import importlib.machinery as machinery_module
+
+        self.assertEqual(machinery_module.__name__, "importlib.machinery")
+
 
 if __name__ == "__main__":
     unittest.main()
