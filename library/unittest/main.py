2e1bb770bae0f82e59c736e7c0691e6dfd63feaa
Max Bernstein <emacs@fb.com>
2019-09-11 19:52:00 -0700

    Remove argparse TODOs from unittest
    Based on Facebook D17250452

--- library/unittest/main.py
+++ library/unittest/main.py
@@ -5,8 +5,7 @@
 # fmt: off
 """Unittest main program"""
 
-# TODO(T42597253): argparse module
-# import argparse
+import argparse
 import os
 import sys
 
@@ -119,28 +118,26 @@ class TestProgram(object):
             print(MODULE_EXAMPLES % {'prog': self.progName})
 
     def parseArgs(self, argv):
-        # TODO(T42597253): argparse module
-        # self._initArgParsers()
-        # if self.module is None:
-        #     if len(argv) > 1 and argv[1].lower() == 'discover':
-        #         self._do_discovery(argv[2:])
-        #         return
-        #     self._main_parser.parse_args(argv[1:], self)
-        #     if not self.tests:
-        #         # this allows "python -m unittest -v" to still work for
-        #         # test discovery.
-        #         self._do_discovery([])
-        #         return
-        # else:
-        #     self._main_parser.parse_args(argv[1:], self)
+        self._initArgParsers()
+        if self.module is None:
+            if len(argv) > 1 and argv[1].lower() == 'discover':
+                self._do_discovery(argv[2:])
+                return
+            self._main_parser.parse_args(argv[1:], self)
+            if not self.tests:
+                # this allows "python -m unittest -v" to still work for
+                # test discovery.
+                self._do_discovery([])
+                return
+        else:
+            self._main_parser.parse_args(argv[1:], self)
 
-        # if self.tests:
-        #     self.testNames = _convert_names(self.tests)
-        #     if __name__ == '__main__':
-        #         # to support python -m unittest ...
-        #         self.module = None
-        # elif self.defaultTest is None:
-        if self.defaultTest is None:
+        if self.tests:
+            self.testNames = _convert_names(self.tests)
+            if __name__ == '__main__':
+                # to support python -m unittest ...
+                self.module = None
+        elif self.defaultTest is None:
             # createTests will load tests from self.module
             self.testNames = None
         elif isinstance(self.defaultTest, str):
@@ -162,72 +159,66 @@ class TestProgram(object):
         self._discovery_parser = self._getDiscoveryArgParser(parent_parser)
 
     def _getParentArgParser(self):
-        # TODO(T42597253): argparse module
-        # parser = argparse.ArgumentParser(add_help=False)
+        parser = argparse.ArgumentParser(add_help=False)
 
-        # parser.add_argument('-v', '--verbose', dest='verbosity',
-        #                     action='store_const', const=2,
-        #                     help='Verbose output')
-        # parser.add_argument('-q', '--quiet', dest='verbosity',
-        #                     action='store_const', const=0,
-        #                     help='Quiet output')
-        # parser.add_argument('--locals', dest='tb_locals',
-        #                     action='store_true',
-        #                     help='Show local variables in tracebacks')
-        # if self.failfast is None:
-        #     parser.add_argument('-f', '--failfast', dest='failfast',
-        #                         action='store_true',
-        #                         help='Stop on first fail or error')
-        #     self.failfast = False
-        # if self.catchbreak is None:
-        #     parser.add_argument('-c', '--catch', dest='catchbreak',
-        #                         action='store_true',
-        #                         help='Catch Ctrl-C and display results so far')
-        #     self.catchbreak = False
-        # if self.buffer is None:
-        #     parser.add_argument('-b', '--buffer', dest='buffer',
-        #                         action='store_true',
-        #                         help='Buffer stdout and stderr during tests')
-        #     self.buffer = False
+        parser.add_argument('-v', '--verbose', dest='verbosity',
+                            action='store_const', const=2,
+                            help='Verbose output')
+        parser.add_argument('-q', '--quiet', dest='verbosity',
+                            action='store_const', const=0,
+                            help='Quiet output')
+        parser.add_argument('--locals', dest='tb_locals',
+                            action='store_true',
+                            help='Show local variables in tracebacks')
+        if self.failfast is None:
+            parser.add_argument('-f', '--failfast', dest='failfast',
+                                action='store_true',
+                                help='Stop on first fail or error')
+            self.failfast = False
+        if self.catchbreak is None:
+            parser.add_argument('-c', '--catch', dest='catchbreak',
+                                action='store_true',
+                                help='Catch Ctrl-C and display results so far')
+            self.catchbreak = False
+        if self.buffer is None:
+            parser.add_argument('-b', '--buffer', dest='buffer',
+                                action='store_true',
+                                help='Buffer stdout and stderr during tests')
+            self.buffer = False
 
-        # return parser
-        pass
+        return parser
 
     def _getMainArgParser(self, parent):
-        # TODO(T42597253): argparse module
-        # parser = argparse.ArgumentParser(parents=[parent])
-        # parser.prog = self.progName
-        # parser.print_help = self._print_help
+        parser = argparse.ArgumentParser(parents=[parent])
+        parser.prog = self.progName
+        parser.print_help = self._print_help
 
-        # parser.add_argument('tests', nargs='*',
-        #                     help='a list of any number of test modules, '
-        #                     'classes and test methods.')
+        parser.add_argument('tests', nargs='*',
+                            help='a list of any number of test modules, '
+                            'classes and test methods.')
 
-        # return parser
-        pass
+        return parser
 
     def _getDiscoveryArgParser(self, parent):
-        # TODO(T42597253): argparse module
-        # parser = argparse.ArgumentParser(parents=[parent])
-        # parser.prog = '%s discover' % self.progName
-        # parser.epilog = ('For test discovery all test modules must be '
-        #                  'importable from the top level directory of the '
-        #                  'project.')
+        parser = argparse.ArgumentParser(parents=[parent])
+        parser.prog = '%s discover' % self.progName
+        parser.epilog = ('For test discovery all test modules must be '
+                         'importable from the top level directory of the '
+                         'project.')
 
-        # parser.add_argument('-s', '--start-directory', dest='start',
-        #                     help="Directory to start discovery ('.' default)")
-        # parser.add_argument('-p', '--pattern', dest='pattern',
-        #                     help="Pattern to match tests ('test*.py' default)")
-        # parser.add_argument('-t', '--top-level-directory', dest='top',
-        #                     help='Top level directory of project (defaults to '
-        #                          'start directory)')
-        # for arg in ('start', 'pattern', 'top'):
-        #     parser.add_argument(arg, nargs='?',
-        #                         default=argparse.SUPPRESS,
-        #                         help=argparse.SUPPRESS)
+        parser.add_argument('-s', '--start-directory', dest='start',
+                            help="Directory to start discovery ('.' default)")
+        parser.add_argument('-p', '--pattern', dest='pattern',
+                            help="Pattern to match tests ('test*.py' default)")
+        parser.add_argument('-t', '--top-level-directory', dest='top',
+                            help='Top level directory of project (defaults to '
+                                 'start directory)')
+        for arg in ('start', 'pattern', 'top'):
+            parser.add_argument(arg, nargs='?',
+                                default=argparse.SUPPRESS,
+                                help=argparse.SUPPRESS)
 
-        # return parser
-        pass
+        return parser
 
     def _do_discovery(self, argv, Loader=None):
         self.start = '.'
