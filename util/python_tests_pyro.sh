ed0f178c1b5bbfbeab6a26dd9e26ef61a7334250
Andrei Talaba <atalaba@fb.com>
2019-09-15 19:52:00 -0700

    Add CPython stdlib test runner
    Summary: This modifies `python_tests_pyro` to run CPython's stdlib tests form a whitelist from inside `third-party/cpython/Lib/tests/`
    
    Based on Facebook D17230869

--- util/python_tests_pyro.sh
+++ util/python_tests_pyro.sh
@@ -25,20 +25,42 @@ if [[ ! -x $PYTHON_BIN ]]; then
     die "$PYTHON_BIN is not executable. Please build using 'make python'"
 fi
 
-FIND_FILTER="*_test.py"
+PYRO_TEST_FILTER="*_test.py"
+TEST_RUNNING_FILTER="*test*.py"
+CPYTHON_TESTS=(
+  test_errno.py
+)
 
 if [[ -n $1 ]]; then
   if [[ ! -f "$SOURCE_DIR/library/$1" ]]; then
     REAL_PATH=$(realpath "$SOURCE_DIR")
-    die "There's no test file named: $1 in $REAL_PATH/library"
+    echo "There's no test file named: $1 in $REAL_PATH/library, checking CPYTHON_TESTS"
+    for i in "${CPYTHON_TESTS[@]}"
+    do
+        if [ "$i" == "$1" ] ; then
+            FOUND=1
+        fi
+    done
+    if [[ ! $FOUND ]]; then
+        die "There's no test file named: $1 in CPYTHON_TESTS"
+    fi
   fi
-  FIND_FILTER="$1"
+  TEST_RUNNING_FILTER="$1"
 fi
 
 cd "$PYRO_BUILD_DIR" || exit 1
 rm -rf tests
 mkdir tests
-find "$SOURCE_DIR/library/" -name "$FIND_FILTER" -exec cp {} tests/ \;
+# Add Pyro tests
+find "$SOURCE_DIR/library/" -name "$PYRO_TEST_FILTER" -exec cp {} tests/ \;
+# Add CPython tests
+for i in "${CPYTHON_TESTS[@]}"; do
+    if [[ -d "$SOURCE_DIR/third-party/cpython/Lib/test/$i" ]]; then
+        die "We don't support running test directories in CPython yet"
+    fi
+    cp "$SOURCE_DIR/third-party/cpython/Lib/test/$i" tests/;
+done
+
 cp "$SOURCE_DIR/library/test_support.py" tests/
 
 if command -v parallel >/dev/null; then
@@ -47,5 +69,5 @@ else
     NUM_CPUS="$(python -c 'import multiprocessing; print(multiprocessing.cpu_count())')"
     TEST_RUNNER=(xargs -t -P "$NUM_CPUS")
 fi
-find "$PYRO_BUILD_DIR/tests/" -name "$FIND_FILTER" -print0 |
+find "$PYRO_BUILD_DIR/tests/" -name "$TEST_RUNNING_FILTER" -print0 |
     PYRO_RECORD_TRACEBACKS=1 "${TEST_RUNNER[@]}" -0 -n1 "$PYTHON_BIN"
