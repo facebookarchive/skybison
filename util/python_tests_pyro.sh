7f69120cc7d3ec139e627695bc44d879f9125223
Matthias Braun <matthiasb@fb.com>
2019-09-17 19:52:00 -0700

    Stop gnu parallel as soon as the first test fails
    Summary: Stop gnu parallel as soon as the first test fails, that makes it less likely to miss errors because they just scroll by on the terminal.
    
    Based on Facebook D17454051

--- util/python_tests_pyro.sh
+++ util/python_tests_pyro.sh
@@ -64,7 +64,7 @@ done
 cp "$SOURCE_DIR/library/test_support.py" tests/
 
 if command -v parallel >/dev/null; then
-    TEST_RUNNER=(parallel --will-cite -v)
+    TEST_RUNNER=(parallel --will-cite -v --halt "now,fail=1")
 else
     NUM_CPUS="$(python -c 'import multiprocessing; print(multiprocessing.cpu_count())')"
     TEST_RUNNER=(xargs -t -P "$NUM_CPUS")
