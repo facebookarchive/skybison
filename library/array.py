d25b46f17ec377f4c260c782f35ea8099f37748b
Brittany Reynoso <breynoso@fb.com>
2019-10-20 19:52:00 -0700

    Stub class for array.array
    Summary: Initial stub class for the array.array implementation
    
    Based on Facebook D18016213

---
+++ library/array.py
@@ -0,0 +1,78 @@
+#!/usr/bin/env python3
+"""Array.array module TODO(T55711876): provide an implemenation"""
+
+_unimplemented = _unimplemented  # noqa: F821
+
+
+class array:
+    def __init__(self, typecode, initializer=None):
+        _unimplemented()
+
+    def typecodes(self):
+        _unimplemented()
+
+    def typecode(self):
+        _unimplemented()
+
+    def itemsize(self):
+        _unimplemented()
+
+    def append(self, value):
+        _unimplemented()
+
+    def buffer_info(self):
+        _unimplemented()
+
+    def byteswap(self):
+        _unimplemented()
+
+    def count(self, value):
+        _unimplemented()
+
+    def extend(self, item):
+        _unimplemented()
+
+    def frombytes(self, string_value):
+        _unimplemented()
+
+    def fromfile(self, file_object, num_items):
+        _unimplemented()
+
+    def fromlist(self, list_in):
+        _unimplemented()
+
+    def fromstring(self):
+        _unimplemented()
+
+    def fromunicode(self, unicode_str):
+        _unimplemented()
+
+    def index(self, value):
+        _unimplemented()
+
+    def insert(self, value, position):
+        _unimplemented()
+
+    def pop(self, index):
+        _unimplemented()
+
+    def remove(self, value):
+        _unimplemented()
+
+    def reverse(self):
+        _unimplemented()
+
+    def tobytes(self):
+        _unimplemented()
+
+    def tofile(self, file_object):
+        _unimplemented()
+
+    def tolist(self):
+        _unimplemented()
+
+    def tostring(self):
+        _unimplemented()
+
+    def tounicode(self):
+        _unimplemented()
