d6b76264ead53e701e1eacfca180602fb7d10cdd
Tyler Kieft <tylerk@fb.com>
2019-11-06 14:05:45 -0800

    Merge Modules/sysmodule-test.cpp into Python/sysmodule-test.cpp
    Summary: These are both testing the C-API for the sys module and there should only be one in ext/Python.
    
    Based on Facebook D18356930

--- ext/Python/sysmodule-test.cpp
+++ ext/Python/sysmodule-test.cpp
@@ -1,5 +1,7 @@
 #include "gtest/gtest.h"
 
+#include <string>
+
 #include "Python.h"
 #include "capi-fixture.h"
 #include "capi-testing.h"
@@ -58,6 +60,79 @@ o = C()
   PyObjectPtr object(moduleGet("__main__", "o"));
   EXPECT_EQ(_PySys_GetSizeOf(object), 10);
   EXPECT_EQ(PyErr_Occurred(), nullptr);
+}
+
+TEST_F(SysModuleExtensionApiTest, WriteStdout) {
+  CaptureStdStreams streams;
+  PySys_WriteStdout("Hello, %s!", "World");
+  EXPECT_EQ(streams.out(), "Hello, World!");
+  EXPECT_EQ(streams.err(), "");
+}
+
+TEST_F(
+    SysModuleExtensionApiTest,
+    WriteStdoutCallsSysStdoutWriteOnExceptionWritesToFallbackAndClearsError) {
+  PyRun_SimpleString(R"(
+import sys
+x = 7
+class C:
+  def write(self, text):
+    global x
+    x = 42
+    raise UserWarning()
+
+sys.stdout = C()
+)");
+  CaptureStdStreams streams;
+  PySys_WriteStdout("a");
+  EXPECT_EQ(streams.out(), "a");
+  EXPECT_EQ(streams.err(), "");
+  ASSERT_EQ(PyErr_Occurred(), nullptr);
+  PyObjectPtr x(moduleGet("__main__", "x"));
+  EXPECT_EQ(PyLong_AsLong(x), 42);
+}
+
+TEST_F(SysModuleExtensionApiTest, WriteStdoutWithSysStdoutNoneWritesToStdout) {
+  PyRun_SimpleString(R"(
+import sys
+sys.stdout = None
+)");
+  CaptureStdStreams streams;
+  PySys_WriteStdout("Hello");
+  EXPECT_EQ(streams.out(), "Hello");
+  EXPECT_EQ(streams.err(), "");
+}
+
+TEST_F(SysModuleExtensionApiTest, WriteStdoutWithoutSysStdoutWritesToStdout) {
+  PyRun_SimpleString(R"(
+import sys
+del sys.stdout
+)");
+  CaptureStdStreams streams;
+  PySys_WriteStdout("Konnichiwa");
+  EXPECT_EQ(streams.out(), "Konnichiwa");
+  EXPECT_EQ(streams.err(), "");
+}
+
+TEST_F(SysModuleExtensionApiTest, WriteStdoutTruncatesLongOutput) {
+  static const int max_out_len = 1000;
+  std::string long_str;
+  for (int i = 0; i < 100; i++) {
+    long_str += "0123456789";
+  }
+  ASSERT_EQ(long_str.size(), max_out_len);
+
+  CaptureStdStreams streams;
+  PySys_WriteStdout("%s hello", long_str.c_str());
+  EXPECT_EQ(streams.out(), long_str + "... truncated");
+  EXPECT_EQ(streams.err(), "");
+}
+
+TEST_F(SysModuleExtensionApiTest, WriteStderr) {
+  CaptureStdStreams streams;
+  PySys_WriteStderr("2 + 2 = %d", 4);
+  EXPECT_EQ(streams.out(), "");
+  EXPECT_EQ(streams.err(), "2 + 2 = 4");
 }
 
 }  // namespace py
