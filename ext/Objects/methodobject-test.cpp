7eeacac7e309856461b7f7d6556c47a3216c937a
Eddie Elizondo <eelizondo@fb.com>
2019-11-05 16:22:49 -0800

    Implement PyCFunction_NewEx
    Summary: Required for Cython
    
    Based on Facebook D18258711

--- ext/Objects/methodobject-test.cpp
+++ ext/Objects/methodobject-test.cpp
@@ -14,4 +14,60 @@ TEST_F(MethodExtensionApiTest, ClearFreeListReturnsZeroPyro) {
   EXPECT_EQ(PyCFunction_ClearFreeList(), 0);
 }
 
+TEST_F(MethodExtensionApiTest, NeCFunctionWithModuleReturnsFunction) {
+  PyObjectPtr module(PyModule_New("mod"));
+  ASSERT_NE(module, nullptr);
+  binaryfunc meth = [](PyObject*, PyObject*) { return PyLong_FromLong(1234); };
+  static PyMethodDef foo_func = {"foo", meth, METH_NOARGS};
+  PyObjectPtr func(PyCFunction_NewEx(&foo_func, nullptr, module));
+  ASSERT_NE(func, nullptr);
+  PyObjectPtr noargs(PyTuple_New(0));
+  PyObjectPtr result(PyObject_Call(func, noargs, nullptr));
+  ASSERT_EQ(PyErr_Occurred(), nullptr);
+  EXPECT_TRUE(isLongEqualsLong(result, 1234));
+}
+
+TEST_F(MethodExtensionApiTest, NewCFunctionWithTypeReturnsFunction) {
+  PyRun_SimpleString(R"(
+class Bar: pass
+)");
+  PyObjectPtr type(moduleGet("__main__", "Bar"));
+  ASSERT_NE(type, nullptr);
+  binaryfunc meth = [](PyObject*, PyObject*) { return PyLong_FromLong(1234); };
+  static PyMethodDef foo_func = {"foo", meth, METH_NOARGS};
+  PyObjectPtr func(PyCFunction_NewEx(&foo_func, type, nullptr));
+  ASSERT_NE(func, nullptr);
+  PyObjectPtr noargs(PyTuple_New(0));
+  PyObjectPtr result(PyObject_Call(func, noargs, nullptr));
+  ASSERT_EQ(PyErr_Occurred(), nullptr);
+  EXPECT_TRUE(isLongEqualsLong(result, 1234));
+}
+
+TEST_F(MethodExtensionApiTest, NewCFunctionWithTypeIsFirstArgument) {
+  PyType_Slot slots[] = {
+      {0, nullptr},
+  };
+  static PyType_Spec spec;
+  spec = {
+      "__main__.C", 0, 0, Py_TPFLAGS_DEFAULT, slots,
+  };
+  PyObjectPtr type(PyType_FromSpec(&spec));
+  ASSERT_NE(type, nullptr);
+  testing::moduleSet("__main__", "C", type);
+
+  binaryfunc meth = [](PyObject* self, PyObject* arg) {
+    EXPECT_EQ(self, arg);
+    return PyLong_FromLong(1234);
+  };
+  static PyMethodDef method = {"testself", meth, METH_O};
+  PyObjectPtr func(PyCFunction_NewEx(&method, type, nullptr));
+
+  testing::moduleSet("__main__", "f", func);
+  PyRun_SimpleString(R"(
+result = f(C)
+)");
+  PyObjectPtr result(testing::moduleGet("__main__", "result"));
+  EXPECT_TRUE(isLongEqualsLong(result, 1234));
+}
+
 }  // namespace py
--- ext/Objects/methodobject.cpp
+++ ext/Objects/methodobject.cpp
@@ -1,13 +1,36 @@
 #include "capi-handles.h"
+#include "function-utils.h"
 #include "runtime.h"
 
 namespace py {
 
 PY_EXPORT int PyCFunction_ClearFreeList() { return 0; }
 
-PY_EXPORT PyObject* PyCFunction_NewEx(PyMethodDef* /* l */, PyObject* /* f */,
-                                      PyObject* /* e */) {
-  UNIMPLEMENTED("PyCFunction_NewEx");
+PY_EXPORT PyObject* PyCFunction_NewEx(PyMethodDef* method, PyObject* self,
+                                      PyObject* module) {
+  DCHECK(self != nullptr || module != nullptr,
+         "PyCFunction_NewEx needs at least a self or module argument");
+  DCHECK(self == nullptr || module == nullptr,
+         "PyCFunction_NewEx can't have both a self and module argument");
+  Thread* thread = Thread::current();
+  HandleScope scope(thread);
+  Object function(&scope, NoneType::object());
+  if (module != nullptr) {
+    DCHECK(PyModule_Check(module), "module must be a module");
+    function = functionFromModuleMethodDef(
+        thread, method->ml_name, bit_cast<void*>(method->ml_meth),
+        method->ml_doc, methodTypeFromMethodFlags(method->ml_flags));
+  } else {
+    Function self_func(
+        &scope,
+        functionFromMethodDef(thread, method->ml_name,
+                              bit_cast<void*>(method->ml_meth), method->ml_doc,
+                              methodTypeFromMethodFlags(method->ml_flags)));
+    // Pass self as the first argument
+    Object self_obj(&scope, ApiHandle::fromPyObject(self)->asObject());
+    function = thread->runtime()->newBoundMethod(self_func, self_obj);
+  }
+  return ApiHandle::newReference(thread, *function);
 }
 
 PY_EXPORT int PyCFunction_GetFlags(PyObject* /* p */) {
