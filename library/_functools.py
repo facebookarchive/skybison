df0677698786d8fd8a9ae2af589bab36cd1e7eb8
Eddie Elizondo <eelizondo@fb.com>
2019-12-04 15:31:10 -0800

    Remove pyro copies of functools.py
    Originally, we had copied functools to patch some of the functionality that we did not support yet. Right now, we are able to fully run the CPython version without modifications.
    
    Furthermore, our 3.6 CPython repo was recently updated to backport (D18807229) the `functools.reduce` implementation from 3.8. This updates our third-party `functools.py` to match what we have in our CPython repo.
    
    Based on Facebook D18807245

--- library/_functools.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""Tools that operate on functions."""
-_Unbound = _Unbound  # noqa: F821
-_unimplemented = _unimplemented  # noqa: F821
-
-
-def reduce(function, sequence, initial=_Unbound):
-    _unimplemented()
