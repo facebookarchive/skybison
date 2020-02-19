3e27ee491eae907fb4fd09eef29f120e7cd04d2b
Carl Shapiro <cshapiro@fb.com>
2020-02-18 16:42:21 -0800

    Remove obsolete variables from the linux CMake cache file
    Based on Facebook D19960055

--- util/linux.cmake
+++ util/linux.cmake
@@ -2,8 +2,6 @@
 # This should be used with the `-C` option. Example:
 #     $ cmake -C util/linux.cmake ..
 
-set(WHOLE_ARCHIVE -Wl,--whole-archive CACHE STRING "")
-set(NO_WHOLE_ARCHIVE -Wl,--no-whole-archive CACHE STRING "")
 set(BZIP2_LIBRARIES bz2 CACHE STRING "")
 set(XZ_LIBRARIES lzma CACHE STRING "")
 set(OPENSSL_PREFIX /usr CACHE STRING "")
