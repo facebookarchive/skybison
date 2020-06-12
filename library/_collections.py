86406149a58818b1ee5ddefa0033caf5ea021146
William Meehan <wmeehan@fb.com>
2020-06-11 19:52:00 -0700

    Partially revert D21977138
    As part of her internship, @chitis will be reimplementing `deque` in native code.
    
    Because the new implementation differs significantly from the existing one,
    we must maintain the old one until enough of the native implementation has been completed.
    
    This diff partially reverts D21977138 to set up the codebase for @chitis to continue her implementation.
    
    Based on Facebook D22025839

--- library/_collections.py
+++ library/_collections.py
@@ -12,12 +12,6 @@ from _builtins import (
 )
 
 
-_DEQUE_NUM_ELTS = 30
-_DEQUE_LEFT_LINK = _DEQUE_NUM_ELTS
-_DEQUE_RIGHT_LINK = _DEQUE_NUM_ELTS + 1
-_DEQUE_BLOCK_SIZE = _DEQUE_NUM_ELTS + 2
-
-
 def _deque_guard(self):
     if not isinstance(self, deque):
         raise TypeError(
@@ -100,40 +94,7 @@ class deque:
             return NotImplemented
 
     def __getitem__(self, index):
-        _deque_guard(self)
-        block, index = deque.__getref(self, index)
-        return block[index]
-
-    def __getref(self, index):
-        if not _int_check(index):
-            index = _index(index)
-        if index >= 0:
-            block = self.left
-            while block:
-                l, r = 0, _DEQUE_NUM_ELTS
-                if block is self.left:
-                    l = self.leftndx  # noqa: E741
-                if block is self.right:
-                    r = self.rightndx + 1
-                span = r - l
-                if index < span:
-                    return block, l + index
-                index -= span
-                block = block[_DEQUE_RIGHT_LINK]
-        else:
-            block = self.right
-            while block:
-                l, r = 0, _DEQUE_NUM_ELTS
-                if block is self.left:
-                    l = self.leftndx  # noqa: E741
-                if block is self.right:
-                    r = self.rightndx + 1
-                negative_span = l - r
-                if index >= negative_span:
-                    return block, r + index
-                index -= negative_span
-                block = block[_DEQUE_LEFT_LINK]
-        raise IndexError("deque index out of range")
+        _unimplemented()
 
     def __gt__(self, other):
         _deque_guard(self)
@@ -150,16 +111,7 @@ class deque:
         return self
 
     def __init__(self, iterable=(), maxlen=None):
-        _deque_guard(self)
-        deque.clear(self)
-        if maxlen is not None:
-            _int_guard(maxlen)
-            if maxlen < 0:
-                raise ValueError("maxlen must be non-negative")
-        self._maxlen = maxlen
-        append = deque.append
-        for elem in iterable:
-            append(self, elem)
+        _unimplemented()
 
     def __iter__(self):
         _deque_guard(self)
@@ -228,74 +180,19 @@ class deque:
         block[index] = value
 
     def _iter_impl(self, original_state, giveup):
-        if self.state != original_state:
-            giveup()
-        block = self.left
-        while block:
-            l, r = 0, _DEQUE_NUM_ELTS
-            if block is self.left:
-                l = self.leftndx  # noqa: E741
-            if block is self.right:
-                r = self.rightndx + 1
-            for elem in block[l:r]:
-                yield elem
-                if self.state != original_state:
-                    giveup()
-            block = block[_DEQUE_RIGHT_LINK]
+        _unimplemented()
 
     def _reversed_impl(self, original_state, giveup):
-        if self.state != original_state:
-            giveup()
-        block = self.right
-        while block:
-            l, r = 0, _DEQUE_NUM_ELTS
-            if block is self.left:
-                l = self.leftndx  # noqa: E741
-            if block is self.right:
-                r = self.rightndx + 1
-            for elem in reversed(block[l:r]):
-                yield elem
-                if self.state != original_state:
-                    giveup()
-            block = block[_DEQUE_LEFT_LINK]
+        _unimplemented()
 
     def append(self, x):
-        _deque_guard(self)
-        self.state += 1
-        self.rightndx += 1
-        if self.rightndx == _DEQUE_NUM_ELTS:
-            newblock = [None] * _DEQUE_BLOCK_SIZE
-            self.right[_DEQUE_RIGHT_LINK] = newblock
-            newblock[_DEQUE_LEFT_LINK] = self.right
-            self.right = newblock
-            self.rightndx = 0
-        self.length += 1
-        self.right[self.rightndx] = x
-        if self._maxlen is not None and self.length > self._maxlen:
-            deque.popleft(self)
+        _unimplemented()
 
     def appendleft(self, x):
-        _deque_guard(self)
-        self.state += 1
-        self.leftndx -= 1
-        if self.leftndx == -1:
-            newblock = [None] * _DEQUE_BLOCK_SIZE
-            self.left[_DEQUE_LEFT_LINK] = newblock
-            newblock[_DEQUE_RIGHT_LINK] = self.left
-            self.left = newblock
-            self.leftndx = _DEQUE_NUM_ELTS - 1
-        self.length += 1
-        self.left[self.leftndx] = x
-        if self._maxlen is not None and self.length > self._maxlen:
-            deque.pop(self)
+        _unimplemented()
 
     def clear(self):
-        _deque_guard(self)
-        self.right = self.left = [None] * _DEQUE_BLOCK_SIZE
-        self.rightndx = _DEQUE_NUM_ELTS // 2  # points to last written element
-        self.leftndx = _DEQUE_NUM_ELTS // 2 + 1
-        self.length = 0
-        self.state = 0
+        _unimplemented()
 
     def count(self, value):
         _deque_guard(self)
@@ -331,48 +228,10 @@ class deque:
         return self._maxlen
 
     def pop(self):
-        _deque_guard(self)
-        if self.left is self.right and self.leftndx > self.rightndx:
-            raise IndexError("pop from an empty deque")
-        x = self.right[self.rightndx]
-        self.right[self.rightndx] = None
-        self.length -= 1
-        self.rightndx -= 1
-        self.state += 1
-        if self.rightndx == -1:
-            prevblock = self.right[_DEQUE_LEFT_LINK]
-            if prevblock is None:
-                # the deque has become empty; recenter instead of freeing block
-                self.rightndx = _DEQUE_NUM_ELTS // 2
-                self.leftndx = _DEQUE_NUM_ELTS // 2 + 1
-            else:
-                prevblock[_DEQUE_RIGHT_LINK] = None
-                self.right[_DEQUE_LEFT_LINK] = None
-                self.right = prevblock
-                self.rightndx = _DEQUE_NUM_ELTS - 1
-        return x
+        _unimplemented()
 
     def popleft(self):
-        _deque_guard(self)
-        if self.left is self.right and self.leftndx > self.rightndx:
-            raise IndexError("pop from an empty deque")
-        x = self.left[self.leftndx]
-        self.left[self.leftndx] = None
-        self.length -= 1
-        self.leftndx += 1
-        self.state += 1
-        if self.leftndx == _DEQUE_NUM_ELTS:
-            prevblock = self.left[_DEQUE_RIGHT_LINK]
-            if prevblock is None:
-                # the deque has become empty; recenter instead of freeing block
-                self.rightndx = _DEQUE_NUM_ELTS // 2
-                self.leftndx = _DEQUE_NUM_ELTS // 2 + 1
-            else:
-                prevblock[_DEQUE_LEFT_LINK] = None
-                self.left[_DEQUE_RIGHT_LINK] = None
-                self.left = prevblock
-                self.leftndx = 0
-        return x
+        _unimplemented()
 
     def remove(self, value):
         _deque_guard(self)
@@ -390,34 +249,7 @@ class deque:
             deque.rotate(self, i)
 
     def reverse(self):
-        _deque_guard(self)
-        leftblock = self.left
-        rightblock = self.right
-        leftindex = self.leftndx
-        rightindex = self.rightndx
-        for _ in range(self.length // 2):
-            # Validate that pointers haven't met in the middle
-            assert leftblock != rightblock or leftindex < rightindex
-
-            # Swap
-            (rightblock[rightindex], leftblock[leftindex]) = (
-                leftblock[leftindex],
-                rightblock[rightindex],
-            )
-
-            # Advance left block/index pair
-            leftindex += 1
-            if leftindex == _DEQUE_NUM_ELTS:
-                leftblock = leftblock[_DEQUE_RIGHT_LINK]
-                assert leftblock is not None
-                leftindex = 0
-
-            # Step backwards with the right block/index pair
-            rightindex -= 1
-            if rightindex == -1:
-                rightblock = rightblock[_DEQUE_LEFT_LINK]
-                assert rightblock is not None
-                rightindex = _DEQUE_NUM_ELTS - 1
+        _unimplemented()
 
     def rotate(self, n=1):
         _deque_guard(self)
