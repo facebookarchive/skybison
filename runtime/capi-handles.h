e07300420a45448c6edb39a2e74a4200f4cc4e8e
Max Bernstein <emacs@fb.com>
2020-10-27 19:52:00 -0700

    Pull inlined ApiHandle functions out of the class declaration
    Summary: This follows our style guide better.
    
    Based on Facebook D24581202

--- runtime/capi-handles.h
+++ runtime/capi-handles.h
@@ -87,13 +87,9 @@ class ApiHandle : public PyObject {
   // Returns the managed object associated with the handle checking for
   static RawObject checkFunctionResult(Thread* thread, PyObject* result);
 
-  static ApiHandle* fromPyObject(PyObject* py_obj) {
-    return static_cast<ApiHandle*>(py_obj);
-  }
+  static ApiHandle* fromPyObject(PyObject* py_obj);
 
-  static ApiHandle* fromPyTypeObject(PyTypeObject* type) {
-    return fromPyObject(reinterpret_cast<PyObject*>(type));
-  }
+  static ApiHandle* fromPyTypeObject(PyTypeObject* type);
 
   // WARNING: This function should be called by the garbage collector.
   // Clear out handles which are not referenced by managed objects or by an
@@ -109,15 +105,8 @@ class ApiHandle : public PyObject {
   // Visit all reference_ members of live ApiHandles.
   static void visitReferences(IdentityDict* handles, PointerVisitor* visitor);
 
-  // Get the object from the handle's reference pointer.
-  inline RawObject asObject() {
-    if (isImmediate(this)) {
-      return RawObject{reinterpret_cast<uword>(this) ^ kImmediateTag};
-    }
-    DCHECK(reference_ != 0 || isManaged(this),
-           "A handle or native instance must point back to a heap instance");
-    return RawObject{reference_};
-  }
+  // Get the object from the handle's reference field.
+  RawObject asObject();
 
   // Return native proxy belonging to an extension object.
   RawNativeProxy asNativeProxy();
@@ -132,44 +121,24 @@ class ApiHandle : public PyObject {
 
   // Returns true if the PyObject* is an immediate ApiHandle or ApiHandle.
   // Otherwise returns false since the PyObject* is an extension object.
-  static bool isManaged(const PyObject* obj) {
-    return isImmediate(obj) || (obj->ob_refcnt & kManagedBit) != 0;
-  }
+  static bool isManaged(const PyObject* obj);
 
   // Returns the reference count of this object by masking the ManagedBit
   // NOTE: This should only be called by the GC.
-  static bool hasExtensionReference(const PyObject* obj) {
-    DCHECK(!isImmediate(obj),
-           "Cannot get hasExtensionReference of immediate handle");
-    return (obj->ob_refcnt & ~kManagedBit) > 0;
-  }
+  static bool hasExtensionReference(const PyObject* obj);
 
   // Increments the reference count of the handle to signal the addition of a
   // reference from extension code.
-  void incref() {
-    if (isImmediate(this)) return;
-    DCHECK((refcnt() & ~kManagedBit) < (kManagedBit - 1),
-           "Reference count overflowed");
-    ++ob_refcnt;
-  }
+  void incref();
 
   // Decrements the reference count of the handle to signal the removal of a
   // reference count from extension code.
-  void decref() {
-    if (isImmediate(this)) return;
-    DCHECK((refcnt() & ~kManagedBit) > 0, "Reference count underflowed");
-    --ob_refcnt;
-  }
+  void decref();
 
   // Returns the number of references to this handle from extension code.
-  word refcnt() {
-    if (isImmediate(this)) return kBorrowedBit;
-    return ob_refcnt;
-  }
+  word refcnt();
 
-  static bool isImmediate(const PyObject* obj) {
-    return (reinterpret_cast<uword>(obj) & kImmediateMask) != 0;
-  }
+  static bool isImmediate(const PyObject* obj);
 
  private:
   // Returns an owned handle for a managed object. If a handle does not already
@@ -198,5 +167,54 @@ class ApiHandle : public PyObject {
 
 static_assert(sizeof(ApiHandle) == sizeof(PyObject),
               "ApiHandle must not add members to PyObject");
+
+inline ApiHandle* ApiHandle::fromPyObject(PyObject* py_obj) {
+  return static_cast<ApiHandle*>(py_obj);
+}
+
+inline ApiHandle* ApiHandle::fromPyTypeObject(PyTypeObject* type) {
+  return fromPyObject(reinterpret_cast<PyObject*>(type));
+}
+
+inline RawObject ApiHandle::asObject() {
+  if (isImmediate(this)) {
+    return RawObject{reinterpret_cast<uword>(this) ^ kImmediateTag};
+  }
+  DCHECK(reference_ != 0 || isManaged(this),
+         "A handle or native instance must point back to a heap instance");
+  return RawObject{reference_};
+}
+
+inline bool ApiHandle::isManaged(const PyObject* obj) {
+  return isImmediate(obj) || (obj->ob_refcnt & kManagedBit) != 0;
+}
+
+inline bool ApiHandle::hasExtensionReference(const PyObject* obj) {
+  DCHECK(!isImmediate(obj),
+         "Cannot get hasExtensionReference of immediate handle");
+  return (obj->ob_refcnt & ~kManagedBit) > 0;
+}
+
+inline void ApiHandle::incref() {
+  if (isImmediate(this)) return;
+  DCHECK((refcnt() & ~kManagedBit) < (kManagedBit - 1),
+         "Reference count overflowed");
+  ++ob_refcnt;
+}
+
+inline void ApiHandle::decref() {
+  if (isImmediate(this)) return;
+  DCHECK((refcnt() & ~kManagedBit) > 0, "Reference count underflowed");
+  --ob_refcnt;
+}
+
+inline word ApiHandle::refcnt() {
+  if (isImmediate(this)) return kBorrowedBit;
+  return ob_refcnt;
+}
+
+inline bool ApiHandle::isImmediate(const PyObject* obj) {
+  return (reinterpret_cast<uword>(obj) & kImmediateMask) != 0;
+}
 
 }  // namespace py
