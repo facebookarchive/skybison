3bb82843b25b6b1126fb7f436051741469146d97
Max Bernstein <emacs@fb.com>
2020-11-06 13:30:53 -0800

    Place .pyc files next to benchmarks
    This makes uses of `__file__` in benchmarks like `pyflate` work.
    D24765140 broke `__file__` because the `.pyc`s were in `__pycache__`.
    
    Based on Facebook D24800777

--- benchmarks/_compile_tool.py
+++ benchmarks/_compile_tool.py
@@ -1,7 +1,17 @@
 """Load benchmark module file so bytecode is compiled and cached."""
+import os
 import py_compile
 import runpy
 import sys
+
+
+def pyc_file(filename):
+    """Put the interpreter-tagged .pyc next to the .py so that __file__
+    directives in benchmarks work."""
+    basename = os.path.basename(filename)
+    stem, _ = os.path.splitext(basename)
+    containing_dir = os.path.dirname(filename)
+    return os.path.join(containing_dir, f"{stem}.{sys.implementation.cache_tag}.pyc")
 
 
 def main():
@@ -9,7 +19,7 @@ def main():
     runpy.run_path(main_file)
     # The tool that runs _compile_tool uses the stdout of this process to
     # determine where the result .pyc is located.
-    print(py_compile.compile(main_file))
+    print(py_compile.compile(main_file, pyc_file(main_file)))
 
 
 if __name__ == "__main__":
