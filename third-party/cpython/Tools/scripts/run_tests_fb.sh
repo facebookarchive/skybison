bedb60e056ab96683682d0c709cd936a5d24b056
Matthias Braun <matthiasb@fb.com>
2021-04-26 19:52:00 -0700

    Disable broken SSL tests
    Summary: Disable broken SSL tests.
    
    Based on Facebook D28018157

--- third-party/cpython/Tools/scripts/run_tests_fb.sh
+++ third-party/cpython/Tools/scripts/run_tests_fb.sh
@@ -5,12 +5,23 @@
 set -eux
 
 FILTER=""
-# TODO(T64862519) Remove this
+# TODO(T64862519, T89667735) Reenable ssl tests.
+FILTER+=" | grep -v test.test_ssl.BasicSocketTests.test_match_hostname"
 FILTER+=" | grep -v test.test_ssl.BasicSocketTests.test_openssl_version"
+FILTER+=" | grep -v test.test_ssl.BasicSocketTests.test_parse_all_sans"
+FILTER+=" | grep -v test.test_ssl.BasicSocketTests.test_parse_cert_CVE_2013_4238"
+FILTER+=" | grep -v test.test_ssl.ThreadedTests.test_default_ecdh_curve"
+FILTER+=" | grep -v test.test_ssl.ThreadedTests.test_dh_params"
+FILTER+=" | grep -v test.test_ssl.ThreadedTests.test_no_shared_ciphers"
+FILTER+=" | grep -v test.test_ssl.ThreadedTests.test_npn_protocols"
+FILTER+=" | grep -v test.test_ssl.ThreadedTests.test_parse_cert_CVE_2013_4238"
+FILTER+=" | grep -v test.test_ssl.ThreadedTests.test_session"
+FILTER+=" | grep -v test.test_ssl.ThreadedTests.test_session_handling"
+FILTER+=" | grep -v test.test_ssl.ThreadedTests.test_version_basic"
 # This test is sketchy as it makes assumption about default options that can
 # depend on build environment, ...
 FILTER+=" | grep -v test.test_ssl.ContextTests.test_options"
 eval ./python -m test test_ssl --list-cases $FILTER > test_ssl_cases.txt
 
 ./python -s -m test -x test_ssl "$@" 
-./python -s -m test test_ssl --matchfile test_ssl_cases.txt "$@" 
+./python -s -m test test_ssl --matchfile test_ssl_cases.txt "$@"
